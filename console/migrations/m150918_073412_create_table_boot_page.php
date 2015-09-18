a1bb5cc140d5184bd8bcec9318e271b1a0a4b175
xuechengjaing <xuechengjiang@corp.1jiajie.com>
2015-09-18 16:06:47 +0800

    更改了启动页，启动页城市关联表结构

--- console/migrations/m150918_073412_create_table_boot_page.php
+++ console/migrations/m150918_073412_create_table_boot_page.php
@@ -5,16 +5,26 @@ use yii\db\Migration;
 
 class m150918_073412_create_table_boot_page extends Migration
 {
-    public function up()
-    {
-
+    public function up(){
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB COMMENT=\'启动页表\'';
+        }
+        $this->createTable('{{%boot_page}}', [
+            'id' => Schema::TYPE_PK . ' AUTO_INCREMENT  COMMENT \'编号\'' ,
+            'boot_page_name' => Schema::TYPE_STRING . '(60) DEFAULT NULL COMMENT \'启动页名称\'',
+            'boot_page_ios_img' => Schema::TYPE_STRING . '(255) DEFAULT NULL COMMENT \'ios图片（640*300）\'',
+            'boot_page_url' => Schema::TYPE_STRING. '(255) DEFAULT NULL COMMENT \'启动页URL\'',
+            'boot_page_residence_time' => Schema::TYPE_INTEGER . '(11) DEFAULT 5 COMMENT \'停留时间 单位为：秒 (默认5秒)\'',
+            'boot_page_online_time' => Schema::TYPE_INTEGER. '(11) COMMENT \'上线时间 （时间戳）\'',
+            'boot_page_offline_time' => Schema::TYPE_INTEGER. '(11) COMMENT \'下线时间 （时间戳）\'',
+            'create_time' => Schema::TYPE_INTEGER. '(11) DEFAULT NULL COMMENT \'创建时间\'',
+            'updatetime' => Schema::TYPE_INTEGER . '(11) DEFAULT NULL COMMENT \'编辑时间\'',
+        ], $tableOptions);
     }
 
-    public function down()
-    {
-        echo "m150918_073412_create_table_boot_page cannot be reverted.\n";
-
-        return false;
+    public function down(){
+        $this->dropTable('{{%boot_page}}');
     }
 
     /*
--- console/migrations/m150918_074216_create_table_boot_page_city.php
+++ console/migrations/m150918_074216_create_table_boot_page_city.php
@@ -8,7 +8,7 @@ class m150918_074216_create_table_boot_page_city extends Migration
     public function up(){
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB COMMENT=\'启动页城市关联表\'';
         }
         $this->createTable('{{%boot_page_city}}', [
             'id' => Schema::TYPE_PK . ' AUTO_INCREMENT  COMMENT \'编号\'' ,
