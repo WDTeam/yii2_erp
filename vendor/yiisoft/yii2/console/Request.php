dfe1147273343930c65a7c30453f4767dff257e8
zhengyi <zhengyi@1jiajie.com>
2015-09-19 00:31:37 +0800

    yii migrate 修复

--- vendor/yiisoft/yii2/console/Request.php
+++ vendor/yiisoft/yii2/console/Request.php
@@ -23,10 +23,7 @@ class Request extends \yii\base\Request
     private $_params;
 
 
-    public function setCookieValidationKey()
-    {
-
-    }
+   
     /**
      * Returns the command line arguments.
      * @return array the command line arguments. It does not include the entry script name.
