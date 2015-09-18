d8e636e91042ca7b9d99aee6dbc32d4bcafe4ff6
zhangxugang <zhangxugang@corp.1jiajie.com>
2015-09-18 15:30:05 +0800

    城市管理系列表创建

--- console/migrations/m150918_031530_create_table_city.php
+++ console/migrations/m150918_031530_create_table_city.php
@@ -9,14 +9,14 @@ class m150918_031530_create_table_city extends Migration
     {
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB COMMENT=\'城市表\'';
         }
         $this->createTable('{{%city}}', [
             'id' => Schema::TYPE_PK . ' AUTO_INCREMENT  COMMENT \'编号\'' ,
-            'city_name' => Schema::TYPE_STRING . '(30) NOT NULL COMMENT \'城市名称\'',
-            'city_is_online' => Schema::TYPE_SMALLINT . '(1) NOT NULL COMMENT \'城市是否上线\'',
-            'create_time' => Schema::TYPE_INTEGER. '(11) NOT NULL COMMENT \'创建时间\'',
-            'updatetime' => Schema::TYPE_INTEGER . '(11) NOT NULL COMMENT \'编辑时间\'',
+            'city_name' => Schema::TYPE_STRING . '(30) DEFAULT NULL COMMENT \'城市名称\'',
+            'city_is_online' => Schema::TYPE_SMALLINT . '(1) DEFAULT NULL COMMENT \'城市是否上线\'',
+            'create_time' => Schema::TYPE_INTEGER. '(11) DEFAULT NULL COMMENT \'创建时间\'',
+            'updatetime' => Schema::TYPE_INTEGER . '(11) DEFAULT NULL COMMENT \'编辑时间\'',
         ], $tableOptions);
     }
 
