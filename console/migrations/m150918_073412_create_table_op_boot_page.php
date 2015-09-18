d3041723e2f01d43a8c1ad76934187c4cc534acc
zhangxugang <zhangxugang@corp.1jiajie.com>
2015-09-18 18:22:53 +0800

    运营管理建表

--- console/migrations/m150918_073412_create_table_op_boot_page.php
+++ console/migrations/m150918_073412_create_table_op_boot_page.php
@@ -3,18 +3,19 @@
 use yii\db\Schema;
 use yii\db\Migration;
 
-class m150918_073412_create_table_boot_page extends Migration
+class m150918_073412_create_table_op_boot_page extends Migration
 {
     public function up(){
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB COMMENT=\'启动页表\'';
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB COMMENT=\'启动页表\'';
         }
-        $this->createTable('{{%boot_page}}', [
+        $this->createTable('{{%op_boot_page}}', [
             'id' => Schema::TYPE_PK . ' AUTO_INCREMENT  COMMENT \'编号\'' ,
             'boot_page_name' => Schema::TYPE_STRING . '(60) DEFAULT NULL COMMENT \'启动页名称\'',
-            'boot_page_ios_img' => Schema::TYPE_STRING . '(255) DEFAULT NULL COMMENT \'ios图片（640*300）\'',
-            'boot_page_url' => Schema::TYPE_STRING. '(255) DEFAULT NULL COMMENT \'启动页URL\'',
+            'boot_page_ios_img' => Schema::TYPE_STRING . '(255) DEFAULT NULL COMMENT \'ios图片\'',
+            'boot_page_android_img' => Schema::TYPE_STRING . '(255) DEFAULT NULL COMMENT \'android图片\'',
+            'boot_page_url' => Schema::TYPE_STRING. '(255) DEFAULT NULL COMMENT \'启动页连接地址\'',
             'boot_page_residence_time' => Schema::TYPE_INTEGER . '(11) DEFAULT 5 COMMENT \'停留时间 单位为：秒 (默认5秒)\'',
             'boot_page_online_time' => Schema::TYPE_INTEGER. '(11) COMMENT \'上线时间 （时间戳）\'',
             'boot_page_offline_time' => Schema::TYPE_INTEGER. '(11) COMMENT \'下线时间 （时间戳）\'',
@@ -24,7 +25,7 @@ class m150918_073412_create_table_boot_page extends Migration
     }
 
     public function down(){
-        $this->dropTable('{{%boot_page}}');
+        $this->dropTable('{{%op_boot_page}}');
     }
 
     /*
---
+++ console/migrations/m150918_075922_create_table_op_category_type.php
@@ -0,0 +1,55 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150918_075922_create_table_op_category_type extends Migration
+{
+    public function up(){
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB COMMENT=\'服务类型表\'';
+        }
+        $this->createTable('{{%op_category_type}}', [
+            'id' => Schema::TYPE_PK . ' AUTO_INCREMENT  COMMENT \'编号\'' ,
+            'category_type_name' => Schema::TYPE_STRING . '(60) DEFAULT NULL COMMENT \'服务类型名称\'',
+            'category_id' => Schema::TYPE_INTEGER . '(11) DEFAULT NULL COMMENT \'对应服务品类编号（服务类型的上级编号冗余）\'',
+            'category_name' => Schema::TYPE_STRING . '(11) DEFAULT NULL COMMENT \'对应服务品类名称（服务类型的上级名称冗余）\'',
+            'category_type_introduction' => Schema::TYPE_TEXT . ' DEFAULT NULL COMMENT \'服务类型简介\'',
+            'category_type_english_name' => Schema::TYPE_STRING . '(60) DEFAULT NULL COMMENT \'服务类型英文名称\'',
+            
+            'category_type_start_time' => Schema::TYPE_STRING . '(20) DEFAULT NULL COMMENT \'开始服务时间即上班时间\'',
+            'category_type_end_time' => Schema::TYPE_STRING . '(20) DEFAULT NULL COMMENT \'结束服务时间即下班时间\'',
+            'category_type_service_time_slot' => Schema::TYPE_TEXT . ' DEFAULT NULL COMMENT \'可服务时间段（序列化方式存储）\'',
+            'category_type_service_interval_time' => Schema::TYPE_INTEGER . '(11) DEFAULT NULL COMMENT \'服务间隔时间(单位：秒)\'',
+            
+            'price_strategy_id' => Schema::TYPE_INTEGER . '(11) DEFAULT NULL COMMENT \'价格策略编号\'',
+            'price_strategy_name' => Schema::TYPE_STRING . '(30) DEFAULT NULL COMMENT \'价格策略名称\'',
+            'category_type_price' => Schema::TYPE_MONEY . ' DEFAULT 0 COMMENT \'价格\'',
+            'category_type_balance_price' => Schema::TYPE_MONEY . ' DEFAULT 0 COMMENT \'阿姨结算价格\'',
+            'category_type_additional_cost' => Schema::TYPE_MONEY . ' DEFAULT 0 COMMENT \'附加费用\'',
+            'category_type_lowest_consume' => Schema::TYPE_MONEY . ' DEFAULT 0 COMMENT \'最低消费\'',
+            'category_type_price_description' => Schema::TYPE_TEXT . ' DEFAULT NULL COMMENT \'价格备注\'',
+            'category_type_market_price' => Schema::TYPE_MONEY . ' DEFAULT 0 COMMENT \'市场价格\'',
+            'tags' => Schema::TYPE_TEXT . ' DEFAULT NULL COMMENT \'服务类型标签编号(序列化方式存储)\'',
+            'category_type_app_ico' => Schema::TYPE_TEXT . ' DEFAULT 0 COMMENT \'APP端图标(序列化方式存储|首页大图，首页小图，分类页小图，订单页小图)\'',
+            'category_type_pc_ico' => Schema::TYPE_TEXT . ' DEFAULT 0 COMMENT \'PC端图标(序列化方式存储|首页推荐大图，更多推荐大图，下单页小图)\'',
+            
+        ], $tableOptions);
+    }
+
+    public function down(){
+        $this->dropTable('{{%op_category_type}}');
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
