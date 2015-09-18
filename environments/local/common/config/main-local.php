2f0248ce7660d97a4bd543c926f9e4ae1382bb3d
zhengyi <zhengyi@1jiajie.com>
2015-09-18 18:20:05 +0800

    去掉local环境无用db配置

--- environments/local/common/config/main-local.php
+++ environments/local/common/config/main-local.php
@@ -8,20 +8,7 @@ return [
             'password' => 'devboss',
             'tablePrefix' => 'ejj_',
             'charset' => 'utf8',
-        ],
-        'dbv1' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=rdsh52vh252q033a4ci5.mysql.rds.aliyuncs.com;dbname=sq_ejiajie',
-            'username' => 'sq_ejiajie',
-            'password' => 'test_sq_ejiajie',
-            'charset' => 'utf8',
-        ],
-        'dbv2' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=rdsh52vh252q033a4ci5.mysql.rds.aliyuncs.com;dbname=sq_ejiajie_v2',
-            'username' => 'sq_ejiajie',
-            'password' => 'test_sq_ejiajie',
-            'charset' => 'utf8',
         ]
+      
     ]
 ];
