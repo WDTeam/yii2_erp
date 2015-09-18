1c615274f326553c7eff22878a981a5927b1815c
linhongyou <linhongyou@1jiajie.com>
2015-09-18 22:45:15 +0800

    添加订单相关数据迁移

--- console/migrations/m150918_142056_create_table_order_status_info.php
+++ console/migrations/m150918_142056_create_table_order_status_info.php
@@ -13,21 +13,21 @@ class m150918_142056_create_table_order_status_info extends Migration
         }
 
 
-        $this->createTable('{{%order_status}}', [
-            'id'=> Schema::TYPE_PK.'NOT NULL AUTO_INCREMENT COMMENT \'ID\'',
+        $this->createTable('{{%order_status_info}}', [
+            'id'=> Schema::TYPE_PK.' NOT NULL AUTO_INCREMENT COMMENT \'ID\'',
             'order_status_info_name'=> Schema::TYPE_STRING.'(255) NOT NULL COMMENT \'状态名称\'',
             'order_status_flow'=> Schema::TYPE_STRING.'(32) NOT NULL DEFAULT 0 COMMENT \'大体流程\'',
             'order_status_oper'=> Schema::TYPE_STRING.'(255) NOT NULL COMMENT \'对应操作\'',
             'order_status_oper_man'=> Schema::TYPE_STRING.'(255) NOT NULL COMMENT \'对应的角色\'',
             'created_at' => Schema::TYPE_INTEGER.'(10) unsigned NOT NULL DEFAULT 0',
             'updated_at' => Schema::TYPE_INTEGER.'(10) unsigned NOT NULL DEFAULT 0',
-            'isdel' => Schema::TYPE_SMALLINT.'(1) unsigned NOT NULL DEFAULT',
+            'isdel' => Schema::TYPE_SMALLINT.'(1) unsigned NOT NULL DEFAULT 0',
         ], $tableOptions);
     }
 
     public function down()
     {
-        $this->dropTable('{{%order_status}}');
+        $this->dropTable('{{%order_status_info}}');
     }
 
     /*
