f40e5d5ac9fdc0fae2e973c3c654a8e74f49c77a
高峰 <pangaofeng@1jiajie.com>
2015-09-19 15:11:34 +0800

    潘高峰 1增加表统一日志，修改表字段

--- console/migrations/m150918_125452_create_table_fi_order_channel.php
+++ console/migrations/m150918_125452_create_table_fi_order_channel.php
@@ -3,29 +3,35 @@
 use yii\db\Schema;
 use yii\db\Migration;
 
-class m150918_125452_create_table_order_channel extends Migration
+class m150918_125452_create_table_fi_order_channel extends Migration
 {
     public function up()
     {
 $tableOptions = null;
        if ($this->db->driverName === 'mysql') {
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB COMMENT=\'订单渠道表\'';
-        }
-        $this->createTable('{{%order_channel}}', [
-            'id' => Schema::TYPE_PK . '(5) AUTO_INCREMENT  COMMENT \'主键id\'' ,
+      
+		   $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB COMMENT=\'订单渠道表\'';
+      
+		   }
+    
+		   $this->createTable('{{%fi_order_channel}}', [
+           
+		   'id' => Schema::TYPE_PK . '(5) AUTO_INCREMENT  COMMENT \'主键id\'' ,
             'order_channel_name' => Schema::TYPE_STRING . '(50) DEFAULT NULL COMMENT \'渠道名称\'',
-            'order_channel_sort' => Schema::TYPE_SMALLINT . '(5) DEFAULT 1 COMMENT \'排序\'',
+         'order_channel_sort' => Schema::TYPE_SMALLINT . '(5) DEFAULT 1 COMMENT \'排序\'',
             'order_channel_is_lock' => Schema::TYPE_SMALLINT . '(1) DEFAULT \'1\' COMMENT \'1 上架 2 下架\'',
-            'addtime' => Schema::TYPE_INTEGER. '(10) DEFAULT NULL COMMENT \'增加时间\'',
+         
+		   'create_time' => Schema::TYPE_INTEGER. '(10) DEFAULT NULL COMMENT \'增加时间\'',
             'is_del' => Schema::TYPE_SMALLINT . '(1) DEFAULT \'0\' COMMENT \'0 正常 1 删除\'',
-        ], $tableOptions);
+  
+		   ], $tableOptions);
         
         
 	}
 
     public function down()
     {
-        $this->dropTable('{{%order_channel}}');
+        $this->dropTable('{{%fi_order_channel}}');
 
         return false;
     }
