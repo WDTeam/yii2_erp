a4ffaaac22c21bdb53d528679ba8bb4627bf2a92
刘道强 <liudaoqiang@corp.1jiajie.com>
2015-09-20 22:45:25 +0800

    修改客户相关数据表注释

--- console/migrations/m150918_132943_create_table_channal.php
+++ console/migrations/m150918_132943_create_table_channal.php
@@ -14,10 +14,10 @@ class m150918_132943_create_table_channal extends Migration
         $this->createTable('{{%channal}}', [
             'id'=>  Schema::TYPE_PK.'(8) NOT NULL AUTO_INCREMENT COMMENT \'主键\'' ,
             'channal_name'=>  Schema::TYPE_STRING.'(16) NOT NULL COMMENT \'聚道名称\'' ,
-            'pid'=>  Schema::TYPE_INTEGER.'(8) NULL DEFAULT NULL' ,
-            'created_at'=>  Schema::TYPE_INTEGER.'(11) NOT NULL' ,
-            'updated_at'=>  Schema::TYPE_INTEGER.'(11) NOT NULL' ,
-            'is_del'=>  Schema::TYPE_SMALLINT.'(4) NOT NULL' ,
+            'pid'=>  Schema::TYPE_INTEGER.'(8) DEFAULT 0 COMMENT \'父级id\'' ,
+            'created_at'=>  Schema::TYPE_INTEGER.'(11) NOT NULL COMMENT \'创建时间\'' ,
+            'updated_at'=>  Schema::TYPE_INTEGER.'(11) NOT NULL COMMENT \'更新时间\'' ,
+            'is_del'=>  Schema::TYPE_SMALLINT.'(4) NOT NULL DEFAULT 0 COMMENT \'是否逻辑删除\'' ,
             ], $tableOptions);
     }
 
