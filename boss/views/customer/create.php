f64c3dff4e9e73e174440b2ef38c0391310eb030
刘道强 <liudaoqiang@corp.1jiajie.com>
2015-09-21 00:20:46 +0800

    修改顾客管理页面

--- boss/views/customer/create.php
+++ boss/views/customer/create.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
  * @var common\models\Customer $model
  */
 
-$this->title = Yii::t('boss', '顾客 {modelClass}', [
+$this->title = Yii::t('boss', '新增 {modelClass}', [
     'modelClass' => '顾客',
 ]);
 $this->params['breadcrumbs'][] = ['label' => Yii::t('boss', 'Customers'), 'url' => ['index']];
@@ -15,7 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="customer-create">
     <div class="page-header">
-        <h1><?= Html::encode($this->title) ?></h1>
+        <!-- <h1><?= Html::encode($this->title) ?></h1> -->
     </div>
     <?= $this->render('_form', [
         'model' => $model,
