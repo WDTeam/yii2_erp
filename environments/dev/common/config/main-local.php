bb29527477affb1bea0a89fc4ff7b7b80221cec6
RobertOnMBP <crazyerghb@126.com>
2015-09-20 16:14:39 +0800

    修改了数据库配置文件，绝对不要修改数据库配置， 如果要修改，请修改你的数据库的命名

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -1,26 +1,19 @@
 <?php
+
+/*
+*  绝对绝对绝对不要修改这个文件！！！！！！（郭红波）
+*  绝对绝对绝对不要修改这个文件！！！！！！（郭红波）
+*  绝对绝对绝对不要修改这个文件！！！！！！（郭红波）
+*/
+
 return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=rdsh52vh252q033a4ci5.mysql.rds.aliyuncs.com;dbname=sq_ejiajie_v2',
-            'username' => 'sq_ejiajie',
-            'password' => 'test_sq_ejiajie',
+            'dsn' => 'mysql:host=rdsh52vh252q033a4ci5.mysql.rds.aliyuncs.com;dbname=dev-boss-db',
+            'username' => 'dev_boss_db_dbo',
+            'password' => 'devboss',
             'charset' => 'utf8',
         ],
-        'dbv1' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=rdsh52vh252q033a4ci5.mysql.rds.aliyuncs.com;dbname=sq_ejiajie',
-            'username' => 'sq_ejiajie',
-            'password' => 'test_sq_ejiajie',
-            'charset' => 'utf8',
-        ],
-        'dbv2' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=rdsh52vh252q033a4ci5.mysql.rds.aliyuncs.com;dbname=sq_ejiajie_v2',
-            'username' => 'sq_ejiajie',
-            'password' => 'test_sq_ejiajie',
-            'charset' => 'utf8',
-        ]
     ]
 ];
