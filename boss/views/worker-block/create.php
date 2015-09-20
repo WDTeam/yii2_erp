c4120ef7a2572d25ac71a8b4a7f2a6ea4113fa8d
王子庚 <wangzigeng@corp.1jiajie.com>
2015-09-20 19:16:17 +0800

    添加黑名单

---
+++ boss/views/worker-block/create.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\helpers\Html;
+
+/**
+ * @var yii\web\View $this
+ * @var common\models\WorkerBlock $model
+ */
+
+    $this->title = Yii::t('app', '添加黑名单', [
+    'modelClass' => 'Worker Block',
+]);
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Worker Blocks'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="worker-block-create">
+    <div class="page-header">
+        <h1><?= Html::encode($this->title) ?></h1>
+    </div>
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
---
+++ common/models/WorkerBlock.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%worker_block}}".
+ *
+ * @property integer $id
+ * @property integer $worker_id
+ * @property integer $worker_block_type
+ * @property string $worker_block_reason
+ * @property integer $worker_block_start
+ * @property integer $worker_block_finish
+ * @property integer $created_ad
+ * @property integer $updated_ad
+ * @property integer $admin_id
+ */
+class WorkerBlock extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%worker_block}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['worker_id', 'worker_block_type', 'worker_block_start', 'worker_block_finish', 'created_ad', 'updated_ad', 'admin_id'], 'integer'],
+            [['worker_block_reason'], 'string', 'max' => 16]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', '阿姨封号id'),
+            'worker_id' => Yii::t('app', '阿姨id'),
+            'worker_block_type' => Yii::t('app', '阿姨封号类型'),
+            'worker_block_reason' => Yii::t('app', '阿姨封号原因'),
+            'worker_block_start' => Yii::t('app', '封号开始时间'),
+            'worker_block_finish' => Yii::t('app', '封号结束时间'),
+            'created_ad' => Yii::t('app', '创建时间'),
+            'updated_ad' => Yii::t('app', '最后更新时间'),
+            'admin_id' => Yii::t('app', '管理员id'),
+        ];
+    }
+}
