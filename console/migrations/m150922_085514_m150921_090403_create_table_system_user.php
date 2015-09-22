931db622b3700784bf0d05e10393ef8747985e26
xuechengjaing <xuechengjiang@corp.1jiajie.com>
2015-09-22 17:18:34 +0800

    提交忽略

---
+++ console/migrations/m150922_085514_m150921_090403_create_table_system_user.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150922_085514_m150921_090403_create_table_system_user extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m150922_085514_m150921_090403_create_table_system_user cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
