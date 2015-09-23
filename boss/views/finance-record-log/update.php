774dcee7fe258334b5f4fd5fe9b85d6275365cf7
Pangaofeng <pangaofeng@1jiajie.com>
2015-09-23 20:26:09 +0800

    增加支付渠道管理，订单渠道管理

---
+++ boss/views/finance-record-log/update.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+
+/**
+ * @var yii\web\View $this
+ * @var common\models\FinanceRecordLog $model
+ */
+
+$this->title = Yii::t('boss', 'Update {modelClass}: ', [
+    'modelClass' => 'Finance Record Log',
+]) . ' ' . $model->id;
+$this->params['breadcrumbs'][] = ['label' => Yii::t('boss', 'Finance Record Logs'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = Yii::t('boss', 'Update');
+?>
+<div class="finance-record-log-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
