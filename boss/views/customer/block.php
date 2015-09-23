80d2cfd400ca53dd256af9707becb41a13b0ed17
刘道强 <liudaoqiang@corp.1jiajie.com>
2015-09-23 14:02:28 +0800

    modified customer

--- boss/views/customer/block.php
+++ boss/views/customer/block.php
@@ -1,117 +1,86 @@
-<head>
-<link href="../web/adminlte/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-<link href="../web/adminlte/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-<link href="../web/adminlte/css/ionicons.min.css" rel="stylesheet" type="text/css" />
-<link href="../web/adminlte/css/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
-<link href="../web/adminlte/css/AdminLTE.css" rel="stylesheet" type="text/css" />
-<link href="../../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-<link href="../../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-<link href="../../css/ionicons.min.css" rel="stylesheet" type="text/css" />
-<link href="../../css/AdminLTE.css" rel="stylesheet" type="text/css" />
-<script src="../../js/jquery.min.js"></script>
-<script src="../../js/bootstrap.min.js" type="text/javascript"></script>
-<script src="../../js/AdminLTE/app.js" type="text/javascript"></script>
-<script src="../../js/AdminLTE/demo.js" type="text/javascript"></script>
-</head>
+<?php
 
-<?php 
-echo \yii\widgets\LinkPager::widget([
-    'pagination' => $pages,
-]);
-?> 
+use yii\helpers\Html;
+use kartik\grid\GridView;
+use yii\widgets\Pjax;
 
-<div>
-<aside>
-<div>
-<section class="content">
-    <div class="box-search">
-        城市：<input type="text" name="city"/>
-        加入时间：<?= \yii\jui\DatePicker::widget(['name' => 'date',]) ?>至<?= \yii\jui\DatePicker::widget(['name' => 'date',]) ?>
-        <input type="submit" value="查询"/>
-        <!-- <button class="btn btn-info">查询</button> -->
-    </div>
-    <br/>
-<div class="row">
-<div class="col-xs-12">
-<div class="box">
-    <div class="box-header">
-        <h3 class="box-title">黑名单列表</h3>
-    </div>
-    <div class="box-body table-responsive">
-        <table id="example1" class="table table-bordered table-striped">
-            <thead>
-                <tr>
-                    <th><input type="checkbox" name="customer"/></th>
-                    <th>ID</th>
-                    <th>用户名</th>
-                    <th>电话</th>
-                    <th>地址</th>
-                    <th>加入黑名单原因</th>
-                    <th>加入时间</th>
-                    <th>所有订单</th>
-                    <th>账户余额</th>
-                    <th>投诉</th>
-                    <th>评级</th>
-                    <th>创建时间</th>
-                    <th>操作</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php 
-                foreach($models as $model){
-                ?>
-                    <tr>
-                        <td><input type="checkbox" name="customer"/></td>
-                        <td><?= $model["id"]?></td>
-                        <td><?= $model["customer_name"]?></td>
-                        <td><?= $model["customer_phone"]?></td>
-                        <td><?= $model["customer_live_address_detail"]?></td>
-                        <td><?= $model["customer_del_reason"]?></td>
-                        <td><?= $model["created_at"]?></td>
-                        <td><?= $model["order_num"]?></td>
-                        <td><?= $model["customer_balance"]?></td>
-                        <td><?= $model["customer_level"]?></td>
-                        <td><?= $model["customer_complaint_times"]?></td>
-                        <td><?= $model["customer_is_vip"]?></td>
-                        <td><?= $model["is_del"]?></td>
-                        <td><a href="/customer/cancal_del">取消</></td>
-                    </tr>
-                <?php 
-                }
-                ?>
-                
-            </tbody>
-            
-        </table>
-    </div>
-</div>
-</div>
-</div>
-</section>
-</aside>
-</div>
+/**
+ * @var yii\web\View $this
+ * @var yii\data\ActiveDataProvider $dataProvider
+ * @var boss\models\WorkerSearch $searchModel
+ */
 
+$this->title = Yii::t('app', '顾客管理');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="worker-index">
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-<script src="../web/adminlte/js/jquery.min.js"></script>
-<script src="../web/adminlte/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="../web/adminlte/js/plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
-<script src="../web/adminlte/js/plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
-<script src="../web/adminlte/js/AdminLTE/app.js" type="text/javascript"></script>
-<script src="../web/adminlte/js/AdminLTE/demo.js" type="text/javascript"></script>
-<script type="text/javascript">
-    $(function() {
-        $("#example1").dataTable();
-        $('#example2').dataTable({
-            "bPaginate": true,
-            "bLengthChange": false,
-            "bFilter": false,
-            "bSort": true,
-            "bInfo": true,
-            "bAutoWidth": false
-        });
-    });
-</script>
-    
+    <p>
+        <?php /* echo Html::a(Yii::t('app', 'Create {modelClass}', [
+    'modelClass' => 'Worker',
+]), ['create'], ['class' => 'btn btn-success'])*/ ?>
+    </p>
+
+    <?php Pjax::begin();
+    echo GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'worker_name',
+            'shop_id',
+            'worker_phone',
+            'worker_idcard',
+//            'worker_password',
+//            'worker_photo',
+//            'worker_level',
+//            'worker_auth_status',
+//            'worker_ontrial_status',
+//            'worker_onboard_status',
+//            'worker_work_city',
+//            'worker_work_area',
+//            'worker_work_street',
+//            'worker_work_lng',
+//            'worker_work_lat',
+//            'worker_type',
+            'worker_rule_id',
+//            'worker_is_block',
+//            'worker_is_blacklist',
+            'created_ad',
+//            'updated_ad',
+//            'isdel',
+
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'buttons' => [
+                    'update' => function ($url, $model) {
+                        return Html::a('<span class="glyphicon glyphicon-pencil"></span>', Yii::$app->urlManager->createUrl(['worker/view', 'id' => $model->id, 'edit' => 't']), [
+                            'title' => Yii::t('yii', 'Edit'),
+                        ]);
+                    }
+
+                ],
+            ],
+        ],
+        'responsive' => true,
+        'hover' => true,
+        'condensed' => true,
+        'floatHeader' => true,
+        'panel' => [
+            'heading' => '<h3 class="panel-title"><i class="glyphicon glyphicon-th-list"></i> ' . Html::encode($this->title) . ' </h3>',
+            'type' => 'info',
+            'before' =>
+                Html::a('<i class="glyphicon" ></i>全部顾客', ['index?CustomerSearch'], ['class' => 'btn btn-success', 'style' => 'margin-right:10px']),
+            'after' => Html::a('<i class="glyphicon glyphicon-repeat"></i> Reset List',
+                ['index'],
+                ['class' => 'btn btn-info']),
+            'showFooter' => false
+        ],
+    ]);
+    Pjax::end(); ?>
+
+</div>
 
 
 
--- boss/views/customer/index.php
+++ boss/views/customer/index.php
@@ -1,111 +1,74 @@
-<head>
-<link href="../web/adminlte/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-<link href="../web/adminlte/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-<link href="../web/adminlte/css/ionicons.min.css" rel="stylesheet" type="text/css" />
-<link href="../web/adminlte/css/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
-<link href="../web/adminlte/css/AdminLTE.css" rel="stylesheet" type="text/css" />
-</head>
+<?php
 
-<?php 
-echo \yii\widgets\LinkPager::widget([
-    'pagination' => $pages,
-]);
-?> 
+use yii\helpers\Html;
+use kartik\grid\GridView;
+use yii\widgets\Pjax;
 
-<div>
-<aside>
-<div>
-<section class="content">
-    <div class="box-search">
-        城市：<!-- <select name="city"/></select> --><input type="text" name="city"/>
-        加入时间：<?= \yii\jui\DatePicker::widget(['name' => 'date',]) ?>至<?= \yii\jui\DatePicker::widget(['name' => 'date',]) ?>
-       身份：<input type="text" name="customer_is_vip"/>
-       <input type="text" name="merge"/>
-        <input type="submit" value="查询"/>
-        <!-- <button class="btn btn-info">查询</button> -->
-    </div>
-    <br/>
-<div class="row">
-<div class="col-xs-12">
-<div class="box">
-    <div class="box-header">
-        <h3 class="box-title">顾客列表</h3>
-    </div>
-    <div class="box-body table-responsive">
-        <table id="example1" class="table table-bordered table-striped">
-            <thead>
-                <tr>
-                    <th><input type="checkbox" name="customer"/></th>
-                    <th>ID</th>
-                    <th>用户名</th>
-                    <th>电话</th>
-                    <th>地址</th>
-                    <th>加入黑名单原因</th>
-                    <th>加入时间</th>
-                    <th>所有订单</th>
-                    <th>账户余额</th>
-                    <th>投诉</th>
-                    <th>评级</th>
-                    <th>创建时间</th>
-                    <th>操作</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php 
-                foreach($models as $model){
-                ?>
-                    <tr>
-                        <td><input type="checkbox" name="customer"/></td>
-                        <td><?= $model["id"]?></td>
-                        <td><?= $model["customer_name"]?></td>
-                        <td><?= $model["customer_phone"]?></td>
-                        <td><?= $model["customer_live_address_detail"]?></td>
-                        <td><?= $model["customer_is_vip"]?></td>
-                        <td><?= $model["platform_id"]?></td>
-                        <td><?= $model["channal_id"]?></td>
-                        <td><?= $model["order_num"]?></td>
-                        <td><?= $model["customer_balance"]?></td>
-                        <td><?= $model["customer_complaint_times"]?></td>
-                        <td><?= $model["customer_level"]?></td>
-                        <td><?= $model["created_at"]?></td>
-                        <td><a href="/customer/del">加入黑名单</a></td>
-                    </tr>
-                <?php 
-                }
-                ?>
-                
-            </tbody>
-            
-        </table>
-    </div>
-</div>
-</div>
-</div>
-</section>
-</aside>
-</div>
+/**
+ * @var yii\web\View $this
+ * @var yii\data\ActiveDataProvider $dataProvider
+ * @var boss\models\WorkerSearch $searchModel
+ */
 
+$this->title = Yii::t('app', '顾客管理');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="worker-index">
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-<script src="../web/adminlte/js/jquery.min.js"></script>
-<script src="../web/adminlte/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="../web/adminlte/js/plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
-<script src="../web/adminlte/js/plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
-<script src="../web/adminlte/js/AdminLTE/app.js" type="text/javascript"></script>
-<script src="../web/adminlte/js/AdminLTE/demo.js" type="text/javascript"></script>
-<script type="text/javascript">
-    $(function() {
-        $("#example1").dataTable();
-        $('#example2').dataTable({
-            "bPaginate": true,
-            "bLengthChange": false,
-            "bFilter": false,
-            "bSort": true,
-            "bInfo": true,
-            "bAutoWidth": false
-        });
-    });
-</script>
-    
+    <p>
+        <?php /* echo Html::a(Yii::t('app', 'Create {modelClass}', [
+    'modelClass' => 'Worker',
+]), ['create'], ['class' => 'btn btn-success'])*/ ?>
+    </p>
+
+    <?php Pjax::begin();
+    echo GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'id',
+            'customer_name',
+            'customer_phone',
+            'region_id',
+            'customer_is_vip',
+            'platform_id',
+            'channal_id',
+            'customer_score',
+            'customer_balance',
+            'customer_complaint_times',
+           'created_at',
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'buttons' => [
+                    'update' => function ($url, $model) {
+                        return Html::a('<span class="glyphicon glyphicon-pencil"></span>', Yii::$app->urlManager->createUrl(['worker/view', 'id' => $model->id, 'edit' => 't']), [
+                            'title' => Yii::t('yii', 'Edit'),
+                        ]);
+                    }
+
+                ],
+            ],
+        ],
+        'responsive' => true,
+        'hover' => true,
+        'condensed' => true,
+        'floatHeader' => true,
+        'panel' => [
+            'heading' => '<h3 class="panel-title"><i class="glyphicon glyphicon-th-list"></i> ' . Html::encode($this->title) . ' </h3>',
+            'type' => 'info',
+            'before' =>
+                Html::a('<i class="glyphicon" ></i>黑名单', ['index?CustomerSearch[is_del]=1'], ['class' => 'btn btn-success', 'style' => 'margin-right:10px']),
+            'after' => Html::a('<i class="glyphicon glyphicon-repeat"></i> Reset List',
+                ['index'],
+                ['class' => 'btn btn-info']),
+            'showFooter' => false
+        ],
+    ]);
+    Pjax::end(); ?>
+
+</div>
 
 
 
