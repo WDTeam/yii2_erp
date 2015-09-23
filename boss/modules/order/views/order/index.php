6124fbf04a95725c0b32a32317ecf99873db9424
linhongyou <linhongyou@1jiajie.com>
2015-09-23 20:47:49 +0800

    订单搜索

--- boss/modules/order/views/order/index.php
+++ boss/modules/order/views/order/index.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use kartik\grid\GridView;
 use yii\widgets\Pjax;
+use yii\bootstrap\Modal;
 
 /**
  * @var yii\web\View $this
@@ -14,7 +15,11 @@ $this->title = '订单列表';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="order-index">
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php
+    Modal::begin(['header' => '<h2>Hello world</h2>','toggleButton' =>['label'=>'Search','class' => 'btn btn-success','id'=>'modalButton'],]);
+    echo $this->render('_search', ['model' => $searchModel]);
+    Modal::end();
+    ?>
 
     <p>
         <?php /* echo Html::a('Create Order', ['create'], ['class' => 'btn btn-success'])*/  ?>
@@ -32,57 +37,11 @@ $this->params['breadcrumbs'][] = $this->title;
             'order_is_parent',
             'created_at',
             'updated_at',
-            'order_before_status_dict_id',
-            'order_before_status_name',
-            'order_status_dict_id',
             'order_status_name',
-            'order_flag_send',
-            'order_flag_urgent',
-            'order_flag_exception',
-            'order_service_type_id',
             'order_service_type_name',
-            'order_src_id',
             'order_src_name',
-            'channel_id',
             'order_channel_name',
-            'order_channel_order_num',
-            'customer_id',
             'order_ip',
-            'order_customer_phone',
-            'order_booked_begin_time',
-            'order_booked_end_time',
-            'order_booked_count',
-            'address_id',
-            'order_address',
-            'order_unit_money',
-            'order_money',
-            'order_booked_worker_id',
-            'order_customer_need',
-            'order_customer_memo',
-            'order_cs_memo',
-            'order_pay_type',
-            'pay_channel_id',
-            'order_pay_channel_name',
-            'order_pay_flow_num',
-            'order_pay_money',
-            'order_use_acc_balance',
-            'card_id',
-            'order_use_card_money',
-            'coupon_id',
-            'order_use_coupon_money',
-            'promotion_id',
-            'order_use_promotion_money',
-            'order_lock_status',
-            'worker_id',
-            'worker_type_id',
-            'order_worker_type_name',
-            'order_worker_send_type',
-            'shop_id',
-            'comment_id',
-            'order_customer_hidden',
-            'order_pop_pay_money',
-            'invoice_id',
-            'checking_id',
             'admin_id',
             'isdel',
 
