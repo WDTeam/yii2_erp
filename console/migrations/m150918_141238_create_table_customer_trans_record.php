b4c7d5e593bc1c22ac388795cd6c786794ad5bff
lishengqiang <lishengqiang@1jiajie.com>
2015-09-24 19:17:52 +0800

    提交数据表

--- console/migrations/m150918_141238_create_table_customer_trans_record.php
+++ console/migrations/m150918_141238_create_table_customer_trans_record.php
@@ -20,22 +20,19 @@ class m150918_141238_create_table_customer_trans_record extends Migration
             'customer_trans_record_order_channel'  => Schema::TYPE_INTEGER . '(30) NOT NULL COMMENT \'订单渠道名称\'',
             'pay_channel_id'  => Schema::TYPE_SMALLINT . '(6) unsigned NOT NULL COMMENT \'支付渠道\'',
             'customer_trans_record_pay_channel'  => Schema::TYPE_INTEGER . '(30) NOT NULL COMMENT \'支付渠道名称\'',
-            'customer_trans_record_mode'  => Schema::TYPE_BOOLEAN . '(4) unsigned NOT NULL DEFAULT 0 COMMENT \'交易方式:1消费,2=充值,3=退款,4=赔偿,5=补偿\'',
+            'customer_trans_record_mode'  => Schema::TYPE_BOOLEAN . '(4) unsigned NOT NULL DEFAULT 0 COMMENT \'交易方式:1消费,2=充值,3=退款,4=赔偿\'',
             'customer_trans_record_mode_name'  => Schema::TYPE_SMALLINT . '(20) NOT NULL COMMENT \'交易方式名称\'',
             'customer_trans_record_promo_code_money'  => Schema::TYPE_DECIMAL . '(5,2) unsigned NOT NULL DEFAULT 0 COMMENT \'优惠码金额\'',
             'customer_trans_record_coupon_money'  => Schema::TYPE_DECIMAL . '(5,2) unsigned NOT NULL DEFAULT 0 COMMENT \'优惠券金额\'',
             'customer_trans_record_cash'  => Schema::TYPE_DECIMAL . '(5,2) unsigned NOT NULL DEFAULT 0 COMMENT \'现金支付\'',
-
             'customer_trans_record_pre_pay'  => Schema::TYPE_DECIMAL . '(5,2) unsigned NOT NULL DEFAULT 0 COMMENT \'预付费金额（第三方）\'',
             'customer_trans_record_online_pay'  => Schema::TYPE_DECIMAL . '(5,2) unsigned NOT NULL DEFAULT 0 COMMENT \'在线支付\'',
             'customer_trans_record_online_balance_pay'  => Schema::TYPE_DECIMAL . '(5,2) unsigned NOT NULL DEFAULT 0 COMMENT \'在线余额支付\'',
             'customer_trans_record_online_service_card_on'  => Schema::TYPE_STRING . '(30) NOT NULL DEFAULT 0 COMMENT \'服务卡号\'',
             'customer_trans_record_online_service_card_pay'  => Schema::TYPE_DECIMAL . '(5,2) unsigned NOT NULL DEFAULT 0 COMMENT \'服务卡支付\'',
+            'customer_trans_record_compensate_money'  => Schema::TYPE_DECIMAL . '(8,2) unsigned NOT NULL COMMENT \'补偿金额\'',
             'customer_trans_record_refund_money'  => Schema::TYPE_DECIMAL . '(8,2) unsigned NOT NULL COMMENT \'退款金额\'',
-
             'customer_trans_record_money'  => Schema::TYPE_DECIMAL . '(8,2) unsigned NOT NULL DEFAULT 0 COMMENT \'余额支付\'',
-
-
             'customer_trans_record_order_total_money'  => Schema::TYPE_DECIMAL . '(5,2) unsigned NOT NULL DEFAULT 0 COMMENT \'订单总额\'',
             'customer_trans_record_total_money'  => Schema::TYPE_DECIMAL . '(9,2) unsigned NOT NULL DEFAULT 0 COMMENT \'交易总额\'',
             'customer_trans_record_current_balance'  => Schema::TYPE_DECIMAL . '(8,2) unsigned NOT NULL DEFAULT 0 COMMENT \'当前余额\'',
