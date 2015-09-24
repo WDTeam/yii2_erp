2ccec41d6c6f9bf4119e164556796c48085a0983
weibeinan <weibeinan@1jiajie.com>
2015-09-24 15:33:24 +0800

    update search

--- boss/models/FinanceWorkerOrderIncomeSearch.php
+++ boss/models/FinanceWorkerOrderIncomeSearch.php
@@ -6,6 +6,7 @@ use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use common\models\FinanceWorkerOrderIncome;
+use common\models\Order;
 
 /**
  * FinanceWorkerOrderIncomeSearch represents the model behind the search form about `common\models\FinanceWorkerOrderIncome`.
