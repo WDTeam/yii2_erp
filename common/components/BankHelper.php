22e8669b3c9616d6beaf558e1f09bc2dbe28be68
colee <lidenggao@1jiajie.com>
2015-09-25 21:29:22 +0800

    update

---
+++ common/components/BankHelper.php
@@ -0,0 +1,27 @@
+<?php
+namespace common\components;
+
+use yii\helpers\ArrayHelper;
+class BankHelper
+{
+    /**
+     * 获取所有银行
+     */
+    public static function getBanks()
+    {
+        $str = file_get_contents('./../../common/components/banks.txt');
+        $data = explode("\r\n", $str);
+        foreach ($data as $key=>$row){
+            $data[$key] = explode("\t", $row);
+        }
+        return $data;
+    }
+    /**
+     * 获取银行名称列表
+     */
+    public static function getBankNames()
+    {
+        $data = self::getBanks();
+        return ArrayHelper::map($data, 2, 2);
+    }
+}
