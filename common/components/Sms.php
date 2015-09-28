d7d86b39c3a05f8b60387d31f4cbf2145b879d4d
colee <lidenggao@1jiajie.com>
2015-09-28 14:42:47 +0800

    create sms class

---
+++ common/components/Sms.php
@@ -0,0 +1,35 @@
+<?php
+namespace common\components;
+
+use yii\base\Object;
+class Sms extends Object
+{
+    public $userId='J02356';
+    public $password='556201';
+    
+    /**
+     * 发短信
+     * @param string $mobiles 手机号，逗号分隔，可以是单个号码，最多100个
+     * @param string $msg 内容，120字以内
+     * @param number $iMobiCount    并发数量，最多100
+     * @return mixed
+     */
+    public function send($mobiles, $msg, $iMobiCount=1)
+    {
+        $reqdata = http_build_query([
+            'userId'=>'J02356',
+            'password'=>'556201',
+            'pszMobis'=>$mobiles,
+            'pszMsg'=>$msg,
+            'iMobiCount'=>$iMobiCount,
+            'pszSubPort'=>'*'
+        ]);
+        $url='http://ws.montnets.com:9006/MWGate/wmgw.asmx/MongateCsSpSendSmsNew?'.$reqdata;
+        $ch = \curl_init();
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        $data =  curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+}
