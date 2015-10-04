76e5481fa0cc74c6b6ca9da9d79af89b0f845f26
weibeinan <weibeinan@1jiajie.com>
2015-10-04 22:42:48 +0800

    调整结算查询逻辑

--- boss/controllers/FinanceSettleApplyController.php
+++ boss/controllers/FinanceSettleApplyController.php
@@ -13,6 +13,7 @@ use common\models\FinanceWorkerOrderIncome;
 use common\models\FinanceWorkerNonOrderIncome;
 use boss\models\WorkerSearch;
 use boss\models\FinanceWorkerOrderIncomeSearch;
+use boss\models\FinanceWorkerNonOrderIncomeSearch;
 
 /**
  * FinanceSettleApplyController implements the CRUD actions for FinanceSettleApply model.
@@ -77,39 +78,41 @@ class FinanceSettleApplyController extends BaseAuthController
      * @return type
      */
     public function actionExport(){
+        
         $shopName = "通州门店";
         $settleMonth = "8月";
         $workerType = "全时段";
         $statDes = $shopName.$settleMonth.$workerType.'家政人员订单费用表';
         
         $financeSearchModel = new FinanceSettleApplySearch;
-        $financeSearchModel::find();
-        $baseData = array('shop_name'=>'待定','worker_name'=>'待定','worker_idcard'=>'待定','bank_no'=>'待定');
+        $workerIncomeAndDetailArr = $financeSearchModel->getWorkerIncomeAndDetail();
+        $workerIncomeAndDetail = $workerIncomeAndDetailArr[0];
+        $financeWorkerNonOrderIncomeSearch = new FinanceWorkerNonOrderIncomeSearch;
+        $nonOrderIncomeArr = $financeWorkerNonOrderIncomeSearch->getNonOrderIncomeBySettleApplyId($workerIncomeAndDetail['settleApplyId']);
+        $nonOrderIncomeTypeAndMoneyArr = [];
+        foreach($nonOrderIncomeArr as $nonOrderIncome){
+            $nonOrderIncomeTypeAndMoneyArr["'".$nonOrderIncome['finance_worker_non_order_income_type']."'"] = $nonOrderIncome['finance_worker_non_order_income'];
+        }
+        $workerIncomeAndDetailReal = array_merge($nonOrderIncomeTypeAndMoneyArr,$workerIncomeAndDetail);
+        
+        $baseData = array('shop_name'=>'待定','worker_name'=>'待定','worker_idcard'=>'待定','worker_bank_card'=>'待定');
         //获取当前阿姨所有的补贴id和描述，按id排序
         $allWorkerSubsidyIdAndDes = [['rule_id'=>'1','rule_des'=>'路补'],['rule_id'=>'2','rule_des'=>'晚补'],['rule_id'=>'3','rule_des'=>'全勤奖']];
         $sheetColumnLeter = ['E','F','G','H','I','J'];
         $dynamicSheetHeaders = [];
+        $dynamicSheetValues = [];
         $i = 0;
         foreach ($allWorkerSubsidyIdAndDes as $workerSubsidyIdAndDes){
             $dynamicSheetHeaders[$sheetColumnLeter[$i]]=$workerSubsidyIdAndDes['rule_des'];
-            $baseData[$workerSubsidyIdAndDes['rule_id']]=0.00;
+            $dynamicSheetValues[$sheetColumnLeter[$i]] = "'".$workerSubsidyIdAndDes['rule_id']."'";
+            $baseData["'".$workerSubsidyIdAndDes['rule_id']."'"]=0.00;
             $i++;
         }
         //查询结算申请表与worker表关联查询，之后单独查询非订单收入表，放入一个数组中，与baseData数组做merge操作
-        var_dump($baseData);die;
+        
+        $workerIncomeAndDetailToExcel = array_merge($baseData,$workerIncomeAndDetailReal);
         $data=array(
-            0=>array(
-             'id'=>1001,
-             'username'=>'张飞',
-             'password'=>'123456',
-             'address'=>'三国时高老庄250巷101室'
-            ),
-            1=>array(
-             'id'=>1002,
-             'username'=>'关羽',
-             'password'=>'123456',
-             'address'=>'三国时花果山'
-            ),
+            0=>$workerIncomeAndDetailToExcel
            );
            $objPHPExcel=new \PHPExcel();
            $objPHPExcel->getProperties()->setCreator('http://www.jb51.net')
@@ -128,17 +131,21 @@ class FinanceSettleApplyController extends BaseAuthController
                 $objPHPExcel->setActiveSheetIndex(0)->setCellValue($dynamicSheetHeaderKey.'1',$dynamicSheetHeaderValue);
             }
            $i=2;   
-//           foreach($data as $k=>$v){
-//            $objPHPExcel->setActiveSheetIndex(0)
-//                       ->setCellValue('A'.$i,$v['id'])
-//                       ->setCellValue('B'.$i,$v['username'])
-//                       ->setCellValue('C'.$i,$v['password'])
-//                       ->setCellValue('D'.$i,$v['address']);
-//            $i++;
-//           }
-           $objPHPExcel->getActiveSheet()->setTitle('三年级2班');
+           foreach($data as $k=>$v){
+            $objPHPExcel->setActiveSheetIndex(0)
+                       ->setCellValue('A'.$i,$v['shop_name'])
+                       ->setCellValue('B'.$i,$v['worker_name'])
+                       ->setCellValue('C'.$i,$v['worker_idcard'])
+                       ->setCellValue('D'.$i,$v['worker_bank_card']);
+            foreach ($dynamicSheetHeaders as $dynamicSheetHeaderKey => $dynamicSheetHeaderValue){
+                $objPHPExcel->setActiveSheetIndex(0)->setCellValue($dynamicSheetHeaderKey.$i,$v[$dynamicSheetValues[$dynamicSheetHeaderKey]]);
+            }
+            
+            $i++;
+           }
+           $objPHPExcel->getActiveSheet()->setTitle('结算');
            $objPHPExcel->setActiveSheetIndex(0);
-           $filename=urlencode('学生信息统计表').'_'.date('Y-m-dHis');
+           $filename=urlencode('阿姨结算统计表').'_'.date('Y-m-dHis');
            header('Content-Type: application/vnd.ms-excel');
             header('Content-Disposition: attachment;filename="'.$filename.'.xls"');
             header('Cache-Control: max-age=0');
