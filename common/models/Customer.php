2c915bcadce85f3f49d5805dbc94cc706850a056
刘道强 <liudaoqiang@corp.1jiajie.com>
2015-10-08 17:31:42 +0800

    modify customer model

--- common/models/Customer.php
+++ common/models/Customer.php
@@ -97,19 +97,7 @@ class Customer extends \yii\db\ActiveRecord
         ];
     }
 
-    /**
-     * 客户账户余额获取
-     */
-    static public function getBalance($customer_id)
-    {
-        // $customer_id = \Yii::$app->request->get('customer_id');
-        // \Yii::$app->response->format = Response::FORMAT_JSON;
-
-        $customer = Customer::find()->where([
-            'id'=>$customer_id
-            ])->one();
-        return $customer->customer_balance;
-    }
+    
 
     /**
      * 客户账户余额转入
@@ -121,7 +109,7 @@ class Customer extends \yii\db\ActiveRecord
         // \Yii::$app->response->format = Response::FORMAT_JSON;
 
         $customer = Customer::find()->where(['id'=>$customer_id])->one();
-        $balance = $customer->balance;
+        $balance = $customer->customer_balance;
         $customer->customer_balance = bcadd($balance, $cash);
         $customer->validate();
         if ($customer->hasErrors()) {
@@ -141,7 +129,7 @@ class Customer extends \yii\db\ActiveRecord
         // \Yii::$app->response->format = Response::FORMAT_JSON;
 
         $customer = Customer::find()->where(['id'=>$customer_id])->one();
-        $balance = $customer->balance;
+        $balance = $customer->customer_balance;
         $customer->customer_balance = bcsub($balance, $cash);
         $customer->validate();
         if ($customer->hasErrors()) {
