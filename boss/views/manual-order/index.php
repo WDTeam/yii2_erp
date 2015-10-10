31ad043b068d88f15cfd33cb0ebbc273611dbd84
linhongyou <linhongyou@1jiajie.com>
2015-10-10 20:11:40 +0800

    人工派单界面出来了

--- boss/views/manual-order/index.php
+++ boss/views/manual-order/index.php
@@ -3,7 +3,8 @@
 use yii\helpers\Html;
 use kartik\grid\GridView;
 use yii\widgets\Pjax;
-use yii\widgets\ActiveForm;
+use kartik\widgets\ActiveForm;
+use common\models\Shop;
 
 /**
  * @var yii\web\View $this
@@ -57,18 +58,28 @@ $this->params['breadcrumbs'][] = $this->title;
             </table>
         </div>
         <div class="panel-body">
-            <div class="col-sm-4">
-             <input type="text" placeholder="阿姨姓名或电话..." class="form-control" id="search_kw">
-            </div>
-            <div class="col-sm-1">
-            <button class="btn btn-warning" type="button" id="search_submit">
-                <span class="glyphicon glyphicon-search"></span>搜索
-            </button>
-            </div>
-            <div class="col-sm-7">
-            <button class="btn btn-warning" type="button" id="can_not_assign">
-                <span class="glyphicon glyphicon-ban-circle"></span>无法指派
-            </button>
+            <div class="worker-search">
+
+                <?php $form = ActiveForm::begin([
+                    'type' => ActiveForm::TYPE_VERTICAL,
+                    'action' => ['index'],
+                    'method' => 'get',
+                ]); ?>
+
+                <div class="col-sm-2">
+                    <input type="text" name="WorkerSearch[worker_name]" class="form-control" id="workersearch-worker_name" placeholder="阿姨姓名...">
+                </div>
+                <div class="col-sm-2">
+                    <input type="text" name="WorkerSearch[worker_phone]" class="form-control" id="workersearch-worker_phone" placeholder="阿姨电话...">
+                </div>
+                <div class="col-sm-1">
+                    <?= Html::submitButton('<span class="glyphicon glyphicon-search"></span>'.Yii::t('app', 'Search'), ['class' => 'btn btn-warning']) ?>
+                </div>
+                <div class="col-sm-7">
+                    <?= Html::button('<span class="glyphicon glyphicon-ban-circle"></span>无法指派', ['class' => 'btn btn-warning','id'=>'can_not_assign']) ?>
+                </div>
+                <?php ActiveForm::end(); ?>
+
             </div>
         </div>
         <div class="panel-body">
@@ -76,13 +87,44 @@ $this->params['breadcrumbs'][] = $this->title;
             'dataProvider' => $dataProvider,
             'layout' => "{items}",
             'columns' => [
-                'id',
-                'order_code',
-                'order_parent_id',
-                'order_is_parent',
-                'created_at',
-                 'updated_at',
-                ['class' => 'yii\grid\ActionColumn'],
+                'worker_name',
+                [
+                    'format' => 'raw',
+                    'label' => '门店名称',
+                    'value' => function ($dataProvider) {
+                        if($dataProvider->shop_id && Shop::findOne($dataProvider->shop_id)){
+                            return Shop::findOne($dataProvider->shop_id)->name;
+                        }
+                    }
+                ],
+                'worker_phone',
+                'worker_idcard',
+                [
+                    'format' => 'raw',
+                    'label' => '阿姨类型',
+                    'value' => function ($dataProvider) {
+                        return $dataProvider->worker_type ? '自有' : '非自有';
+                    },
+                    'width' => "100px",
+                ],
+                [
+                    'format' => 'raw',
+                    'label' => '阿姨录入时间',
+                    'value' => function ($dataProvider) {
+                        return date('Y-m-d H:i', $dataProvider->created_ad);
+                    },
+                    'width' => "120px",
+                ],
+                [
+                    'format' => 'raw',
+                    'label' => '操作',
+                    'value' => function ($model) {
+                        return Html::a('已联系', Yii::$app->urlManager->createUrl(['worker/view', 'id' => $model->id, 'edit' => 't']), [
+                            'title' => Yii::t('yii', '已联系'),
+                        ]);;
+                    },
+                    'width' => "120px",
+                ]
             ],
         ]); ?>
         </div>
