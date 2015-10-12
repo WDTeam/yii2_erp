e941b855cf99440127f1d65d7f99ef402fdc9cef
xuechengjaing <xuechengjiang@corp.1jiajie.com>
2015-10-12 20:47:55 +0800

    增加城市上线

--- core/models/Operation/CoreOperationShopDistrictGoods.php
+++ core/models/Operation/CoreOperationShopDistrictGoods.php
@@ -10,7 +10,7 @@ class CoreOperationShopDistrictGoods extends CommonOperationShopDistrictGoods
     /**
      * 插入城市商圈商品
      */
-    public static function handleReleaseCity($cityinfo, $shopdistrictinfo, $goodinfo){
+    public static function handleReleaseCity($cityinfo, $shopdistrictinfo, $goodinfo, $settinghopdistrictgoods){
         $cityid = $cityinfo[0];  //商圈id
         $cityname = $cityinfo[1]; //商圈名称
         $shop_district_goods_data = array();
@@ -48,6 +48,7 @@ class CoreOperationShopDistrictGoods extends CommonOperationShopDistrictGoods
         $i = 0;
         foreach((array)$goodinfo['goodids'] as $key => $value){
             $goodsid = $value;
+//            self::delCityShopDistrictGoods($goodsid, $cityid);
             foreach((array)$shopdistrictinfo as $k => $v){
                 $shop_district = explode('-', $v);
                 $shop_district_goods_data[$i][] = $goodinfo['goodscontent'][$goodsid]['operation_goods_name'];  //商品名称
@@ -62,19 +63,27 @@ class CoreOperationShopDistrictGoods extends CommonOperationShopDistrictGoods
                 $shop_district_goods_data[$i][] = $goodinfo['goodscontent'][$goodsid]['operation_category_name'];  //对应服务品类名称（所属分类名称冗余）
                 $shop_district_goods_data[$i][] = $goodinfo['goodscontent'][$goodsid]['operation_goods_introduction'];  //服务类型简介
                 $shop_district_goods_data[$i][] = $goodinfo['goodscontent'][$goodsid]['operation_goods_english_name'];  //商品英文名称
-                $shop_district_goods_data[$i][] = $goodinfo['operation_goods_start_time'][$key];  //服务开始时间
-                $shop_district_goods_data[$i][] = $goodinfo['operation_goods_end_time'][$key];  //服务结束时间
+                
+                $shop_district_goods_data[$i][] = $settinghopdistrictgoods['operation_goods_start_time'][$k];  //服务开始时间
+                $shop_district_goods_data[$i][] = $settinghopdistrictgoods['operation_goods_end_time'][$k];  //服务结束时间
+                
                 $shop_district_goods_data[$i][] = $goodinfo['goodscontent'][$goodsid]['operation_goods_service_interval_time'];  //服务间隔时间(单位：分钟)
                 $shop_district_goods_data[$i][] = $goodinfo['goodscontent'][$goodsid]['operation_goods_service_estimate_time'];  //预计服务时长(单位：分钟)
                 $shop_district_goods_data[$i][] = $goodinfo['goodscontent'][$goodsid]['operation_spec_info'];  //规格id
                 $shop_district_goods_data[$i][] = $goodinfo['goodscontent'][$goodsid]['operation_spec_strategy_unit'];  //计量单位
-                $shop_district_goods_data[$i][] = $goodinfo['operation_goods_price'][$key];  //售价
+                
+                $shop_district_goods_data[$i][] = $settinghopdistrictgoods['operation_goods_price'][$k];  //售价
+                
                 $shop_district_goods_data[$i][] = $goodinfo['goodscontent'][$goodsid]['operation_goods_balance_price'];   //阿姨结算价格
                 $shop_district_goods_data[$i][] = $goodinfo['goodscontent'][$goodsid]['operation_goods_additional_cost'];  //附加费用
-                $shop_district_goods_data[$i][] = $goodinfo['operation_goods_price'][$key]*$goodinfo['operation_goods_lowest_consume'][$key];  //最低消费价格
-                $shop_district_goods_data[$i][] = $goodinfo['operation_goods_lowest_consume'][$key];  //最低消费数量
+                
+                $shop_district_goods_data[$i][] = $settinghopdistrictgoods['operation_goods_price'][$k]*$settinghopdistrictgoods['operation_goods_lowest_consume'][$k];  //最低消费价格
+                $shop_district_goods_data[$i][] = $settinghopdistrictgoods['operation_goods_lowest_consume'][$k];  //最低消费数量
+                
                 $shop_district_goods_data[$i][] = $goodinfo['goodscontent'][$goodsid]['operation_goods_price_description'];  //价格备注
-                $shop_district_goods_data[$i][] = $goodinfo['operation_goods_market_price'][$key];  //市场价格
+                
+                $shop_district_goods_data[$i][] = $settinghopdistrictgoods['operation_goods_market_price'][$k];  //市场价格
+                
                 $shop_district_goods_data[$i][] = $goodinfo['goodscontent'][$goodsid]['operation_tags'];  //个性标签
                 $shop_district_goods_data[$i][] = $goodinfo['goodscontent'][$goodsid]['operation_goods_img'];  //商品图片
                 $shop_district_goods_data[$i][] = time(); //创建时间
@@ -99,5 +108,27 @@ class CoreOperationShopDistrictGoods extends CommonOperationShopDistrictGoods
         }else{
             return self::find()->select(['operation_goods_id', 'operation_shop_district_goods_name', 'operation_shop_district_goods_introduction', 'operation_shop_district_goods_price', 'operation_shop_district_goods_lowest_consume_num', 'operation_shop_district_goods_lowest_consume', 'operation_shop_district_goods_market_price', 'created_at'])->asArray()->where(['operation_city_id' => $city_id, 'operation_shop_district_id' => $shop_district])->All();
         }
+    }
+    
+    /**
+     * 查询城市下面的所有商品
+     * @param type $city_id
+     * @return string
+     */
+    public static function getCityShopDistrictGoodsList($city_id = ''){
+        if(empty($city_id)){
+            return '';
+        }else{
+            return self::find()->where(['operation_city_id' => $city_id])->groupBy('operation_goods_id');
+        }
+    }
+    
+    /**
+     * 删除城市下边商品
+     * @param type $goods_id
+     * @param type $city_id
+     */
+    public static function delCityShopDistrictGoods($goods_id, $city_id){
+        return self::deleteAll(['operation_goods_id' => $goods_id, 'operation_city_id' => $city_id]);
     }
 }
