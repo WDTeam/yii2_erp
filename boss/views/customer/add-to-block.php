01e7e1c4eab0196734012cc22a94a363fb91f7d2
刘道强 <liudaoqiang@corp.1jiajie.com>
2015-10-13 18:15:20 +0800

    modify customer module files

--- boss/views/customer/add-to-block.php
+++ boss/views/customer/add-to-block.php
@@ -1,36 +1,34 @@
 <?php
+
 use yii\helpers\Html;
 use kartik\widgets\ActiveForm;
 use kartik\builder\Form;
 use kartik\datecontrol\DateControl;
-use kartik\date\DatePicker;
-use kartik\widgets\Select2; // or kartik\select2\Select2
-use kartik\field\FieldRange;
+use common\models\Customer;
+use boss\components\AreaCascade;
+
+/**
+ * @var yii\web\View $this
+ * @var boss\models\ShopManager $model
+ * @var yii\widgets\ActiveForm $form
+ */
 ?>
 
-<div class="">
+<div class="customer-add-to-block">
 
-<?php /*$form = ActiveForm::begin([
+<?php $form = ActiveForm::begin([
     'type'=>ActiveForm::TYPE_HORIZONTAL,
-    'action'=>['vacation-create', 'worker_id'=>$workerModel->id]
-]); */ ?>
-    <!--<div class="form-group">
-        <div class="col-sm-offset-0 col-sm-12">
-            <?php // Html::submitButton('确认',['class'=>'btn btn-primary btn-lg btn-block']);?>
-        </div>
-    </div>-->
-    <?php /* ActiveForm::end(); */?>
-<?php
-    $form = ActiveForm::begin([
-        'type' => ActiveForm::TYPE_HORIZONTAL,
-        'formConfig' => ['labelSpan' => 3, 'deviceSize' => ActiveForm::SIZE_SMALL],
-        //'action'=>['block-create', 'id'=>$workerBlockmodel->id]
-    ]);
-    echo $form->field($customerModel, 'customer_name')->textInput(['disabled' => true]);
-    echo $form->field($customerModel, 'customer_phone')->textInput(['disabled' => true]);
-    echo $form->field($customerModel, 'customer_del_reason')->textArea();
-    echo Html::submitButton('确认',['class'=>'btn btn-primary btn-lg btn-block']);
-    ActiveForm::end();
-?>
+    'action'=>['add-to-block', 'id'=>$model->id]
+]); ?>
 
+    <?php echo Html::textarea('customer_del_reason', '', [
+        'style'=>'width:100%'
+    ])?>
+
+    <div class="form-group">
+        <div class="col-sm-offset-0 col-sm-12">
+            <?= Html::submitButton('确认',['class'=>'btn btn-primary btn-lg btn-block']);?>
+        </div>
+    </div>
+<?php ActiveForm::end(); ?>
 </div>
