705d199db4badc5b93baaa9479e20dd3cf23736e
Pangaofeng <pangaofeng@1jiajie.com>
2015-10-16 17:02:53 +0800

    增加初始化上传目录自动构建

--- console/migrations/m150918_135715_create_table_finance_pay_channel.php
+++ console/migrations/m150918_135715_create_table_finance_pay_channel.php
@@ -20,20 +20,20 @@ class m150918_135715_create_table_finance_pay_channel extends Migration
         ], $tableOptions);
 
         $this->execute(
-            "INSERT INTO {{%finance_pay_channel}} VALUES ('1', '三快', 1, 1, 0, '0');
-             INSERT INTO {{%finance_pay_channel}} VALUES ('2', '汉海', 2, 1, 0, '0');
-             INSERT INTO {{%finance_pay_channel}} VALUES ('3', '京东后台', 3, 1, 0, '0');
-             INSERT INTO {{%finance_pay_channel}} VALUES ('4', '快钱', 4, 1, 0, '0');
-             INSERT INTO {{%finance_pay_channel}} VALUES ('5', '支付宝(ejiajiefw)', 5, 1, 0, '0');
-             INSERT INTO {{%finance_pay_channel}} VALUES ('6', '支付宝(47622990)', 6, 1, 0, '0');
-             INSERT INTO {{%finance_pay_channel}} VALUES ('7', '支付宝（1jjtb）', 7, 1, 0, '0');
-             INSERT INTO {{%finance_pay_channel}} VALUES ('8', '百度钱包', 8, 1, 0, '0');
-             INSERT INTO {{%finance_pay_channel}} VALUES ('9', '到位后台', 9, 1, 0, '0');
-             INSERT INTO {{%finance_pay_channel}} VALUES ('10', '微信后台', 10, 1, 0, '0');
-             INSERT INTO {{%finance_pay_channel}} VALUES ('11', '财付通', 11, 1, 0, '0');
-             INSERT INTO {{%finance_pay_channel}} VALUES ('12', '银联后台', 12, 1, 0, '0');
-             INSERT INTO {{%finance_pay_channel}} VALUES ('13', '现金', 13, 1, 0, '0');
-             INSERT INTO {{%finance_pay_channel}} VALUES ('14', '管理后台', 14, 1, 0, '0');"
+            "INSERT INTO {{%finance_pay_channel}} VALUES ('1', '三快', 1, 1, 1443339882, '0');
+             INSERT INTO {{%finance_pay_channel}} VALUES ('2', '汉海', 2, 1, 1443339882, '0');
+             INSERT INTO {{%finance_pay_channel}} VALUES ('3', '京东后台', 3, 1, 1443339882, '0');
+             INSERT INTO {{%finance_pay_channel}} VALUES ('4', '快钱', 4, 1, 1443339882, '0');
+             INSERT INTO {{%finance_pay_channel}} VALUES ('5', '支付宝(ejiajiefw)', 5, 1, 1443339882, '0');
+             INSERT INTO {{%finance_pay_channel}} VALUES ('6', '支付宝(47622990)', 6, 1, 1443339882, '0');
+             INSERT INTO {{%finance_pay_channel}} VALUES ('7', '支付宝（1jjtb）', 7, 1, 1443339882, '0');
+             INSERT INTO {{%finance_pay_channel}} VALUES ('8', '百度钱包', 8, 1,1443339882, '0');
+             INSERT INTO {{%finance_pay_channel}} VALUES ('9', '到位后台', 9, 1, 1443339882, '0');
+             INSERT INTO {{%finance_pay_channel}} VALUES ('10', '微信后台', 10, 1, 1443339882, '0');
+             INSERT INTO {{%finance_pay_channel}} VALUES ('11', '财付通', 11, 1, 1443339882, '0');
+             INSERT INTO {{%finance_pay_channel}} VALUES ('12', '银联后台', 12, 1,1443339882, '0');
+             INSERT INTO {{%finance_pay_channel}} VALUES ('13', '现金', 13, 1,1443339882, '0');
+             INSERT INTO {{%finance_pay_channel}} VALUES ('14', '管理后台', 14, 1,1443339882, '0');"
         );
 
     }
