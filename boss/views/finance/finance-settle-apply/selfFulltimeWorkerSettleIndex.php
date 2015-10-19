5787bd9d8c359b3a4ee923101eeacfc21f9a9d67
weibeinan <weibeinan@1jiajie.com>
2015-10-19 11:04:26 +0800

    调整详情页

--- boss/views/finance/finance-settle-apply/selfFulltimeWorkerSettleIndex.php
+++ boss/views/finance/finance-settle-apply/selfFulltimeWorkerSettleIndex.php
@@ -73,7 +73,7 @@ $this->params['review_section'] = $searchModel->review_section;
                 'template' =>'{view} {agree} {disagree}',
                 'buttons' => [
                     'view' => function ($url, $model) {
-                        return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', Yii::$app->urlManager->createUrl(['/finance/finance-settle-apply/self-fulltime-worker-settle-view', 'id' => $model->id],[]), [
+                        return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', Yii::$app->urlManager->createUrl(['/finance/finance-settle-apply/self-fulltime-worker-settle-view', 'FinanceSettleApplySearch[id]' => $model->id],[]), [
                             'title' => Yii::t('yii', '查看'),'data-pjax'=>'0','target' => '_blank',
                         ]);
                     },
