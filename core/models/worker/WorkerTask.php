385cadfbae76bd0023a340b4177c947d4fc08c06
colee <lidenggao@1jiajie.com>
2015-10-20 23:50:48 +0800

    update task

--- core/models/worker/WorkerTask.php
+++ core/models/worker/WorkerTask.php
@@ -139,6 +139,8 @@ class WorkerTask extends \common\models\WorkerTask
         }
         return $res;
     }
+    
+    
     /**
      * 计算符合阿姨条件的任务列表
      */
@@ -155,6 +157,35 @@ class WorkerTask extends \common\models\WorkerTask
         ->andFilterWhere(['>','worker_task_end', $cur_time])
         ->all();
         return $tasks;
+    }
+    /**
+     * 自动生成阿姨任务
+     */
+    public static function autoCreateTaskLog($worker_id)
+    {
+        $data = [];
+        $tasks = (array)self::getTaskListByWorkerId($worker_id);
+        foreach($tasks as $task){
+            $log = WorkerTaskLog::find()->where([
+                'worker_id'=>$worker_id,
+                'worker_task_id'=>$task->id,
+            ])->one();
+            if(empty($log)){
+                $log = new WorkerTaskLog();
+            }
+            $log->setAttributes([
+                'worker_id'=>$worker_id,
+                'worker_task_id'=>$task->id,
+                'worker_task_name'=>$task->worker_task_name,
+                'worker_task_start'=>$task->worker_task_start,
+                'worker_task_end'=>$task->worker_task_end,
+                'worker_task_reward_type'=>$task->worker_task_reward_type,
+                'worker_task_reward_value'=>$task->worker_task_reward_value,
+            ]);
+            $log->save();
+            $data[] = $log;
+        }
+        return $data;
     }
     /**
      * 给定数据判断是否完成
