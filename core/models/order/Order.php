507e7761a3288843c6737fad90c00aaf712130eb
linhongyou <linhongyou@1jiajie.com>
2015-10-22 15:56:06 +0800

    修改了移除订单池方式

--- core/models/order/Order.php
+++ core/models/order/Order.php
@@ -187,7 +187,9 @@ class Order extends OrderModel
                 $redis_order = $v;
             }
         }
-        Yii::$app->redis->executeCommand('zrem', [self::WAIT_ASSIGN_ORDERS_POOL, $redis_order]);
+        if(!empty($redis_order)) {
+            Yii::$app->redis->executeCommand('zrem', [self::WAIT_ASSIGN_ORDERS_POOL, $redis_order]);
+        }
     }
 
     /**
@@ -223,6 +225,8 @@ class Order extends OrderModel
             }else{ //系统指派失败
                 self::sysAssignUndone($order_id);
             }
+        }else{
+            self::remOrderToPool($order_id);
         }
         $order = OrderSearch::getOne($order_id);
         return ['order_id'=>$order->id,'created_at'=>$order->created_at,'sms'=>$order->orderExtFlag->order_flag_worker_sms,'jpush'=>$order->orderExtFlag->order_flag_worker_jpush,'ivr'=>$order->orderExtFlag->order_flag_worker_ivr];
@@ -336,10 +340,8 @@ class Order extends OrderModel
         $order->admin_id=1;
         if($order->orderExtStatus->order_status_dict_id == OrderStatusDict::ORDER_SYS_ASSIGN_START) { //开始系统指派的订单
             if (OrderStatus::sysAssignUndone($order, [])) {
-                if(!empty($redis_order)){
-                    //把订单从订单池中移除
-                    self::remOrderToPool($order_id);
-                }
+                //把订单从订单池中移除
+                self::remOrderToPool($order_id);
                 return true;
             }
         }
