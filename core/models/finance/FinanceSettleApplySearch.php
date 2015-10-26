8b136067f0812c6af1cd36a4decb79ab22ece6b7
weibeinan <weibeinan@1jiajie.com>
2015-10-26 18:06:37 +0800

    调用任务接口

--- core/models/finance/FinanceSettleApplySearch.php
+++ core/models/finance/FinanceSettleApplySearch.php
@@ -9,6 +9,7 @@ use common\models\finance\FinanceSettleApply;
 use core\models\worker\Worker;
 use core\models\order\Order;
 use common\models\OrderExtWorker;
+use core\models\worker\WorkerTask;
 
 /**
  * FinanceSettleApplySearch represents the model behind the search form about `common\models\finance\FinanceSettleApply`.
@@ -201,6 +202,15 @@ class FinanceSettleApplySearch extends FinanceSettleApply
         $apply_money = 0;//本次应付合计
         $order_noncash_count = 0;//非现金订单
         $order_money_except_cash = 0;//工时费应结，扣除了现金
+        if(($this->worker_type_id ==self::SELF_OPERATION ) && ($this->worker_identity_id == self::FULLTIME)){
+            $this->finance_settle_apply_starttime = self::getFirstDayOfSpecifiedMonth();//结算开始日期
+            $this->finance_settle_apply_endtime = self::getLastDayOfSpecifiedMonth();//结算截止日期
+        }else{
+            $this->finance_settle_apply_starttime = self::getFirstDayOfLastWeek();//结算开始日期
+            $this->finance_settle_apply_endtime = self::getLastDayOfLastWeek();//结算截止日期
+        }
+        $apply_task_count = $this->getTaskAwardCount(12, -1, 100000000000);
+        $apply_task_money = $this->getTaskAwardMoney(12, -1, 100000000000);
         if(count($orders) > 0){
            $order_count = count($orders);
            foreach($orders as $order){
@@ -244,20 +254,58 @@ class FinanceSettleApplySearch extends FinanceSettleApply
             $this->worker_identity_id = $workerInfo['worker_identity_id'];
             $this->worker_type_name = $this->getWorkerTypeName($workerInfo['worker_type']);
             $this->worker_identity_name = $this->getWorkerIdentityDes($workerInfo['worker_identity_id']);
+            $this->shop_id = $workerInfo['shop_id'];
+            $this->shop_name = $workerInfo['shop_name'];
+            $this->shop_manager_name = $workerInfo['shop_manager_name'];
         }
         $this->finance_settle_apply_cycle = $this->getSettleCycleIdByWorkerType($this->worker_type_id, $this->worker_identity_id);//结算周期Id
         $this->finance_settle_apply_cycle_des = $this->getSettleCycleByWorkerType($this->worker_type_id, $this->worker_identity_id);//结算周期描述
         $this->finance_settle_apply_status = FinanceSettleApply::FINANCE_SETTLE_APPLY_STATUS_INIT;//提交结算申请
-        if(($this->worker_type_id ==self::SELF_OPERATION ) && ($this->worker_identity_id == self::FULLTIME)){
-            $this->finance_settle_apply_starttime = self::getFirstDayOfSpecifiedMonth();//结算开始日期
-            $this->finance_settle_apply_endtime = self::getLastDayOfSpecifiedMonth();//结算截止日期
-        }else{
-            $this->finance_settle_apply_starttime = self::getFirstDayOfLastWeek();//结算开始日期
-            $this->finance_settle_apply_endtime = self::getLastDayOfLastWeek();//结算截止日期
-        }
+       
         $this->created_at = time();//申请创建时间
         return $this;
 
+    }
+    
+    /**
+     * 获取任务奖励金额
+     * @param type $workerId
+     * @param type $finance_settle_apply_starttime
+     * @param type $finance_settle_apply_endtime
+     * @return type
+     */
+    public function getTaskAwardMoney($workerId,$finance_settle_apply_starttime,$finance_settle_apply_endtime){
+        $taskAwardMoney = 0;
+        $taskAwardList = $this->getTaskAwardList($workerId, $finance_settle_apply_starttime, $finance_settle_apply_endtime);
+        foreach($taskAwardList as $taskAward){
+            $taskAwardMoney += $taskAward ->worker_task_reward_value;
+        }
+        return $taskAwardMoney;
+    }
+    
+    /**
+     * 获取任务数
+     * @param type $workerId
+     * @param type $finance_settle_apply_starttime
+     * @param type $finance_settle_apply_endtime
+     * @return type
+     */
+    public function getTaskAwardCount($workerId,$finance_settle_apply_starttime,$finance_settle_apply_endtime){
+        $taskAwardList = $this->getTaskAwardList($workerId, $finance_settle_apply_starttime, $finance_settle_apply_endtime);
+        var_dump($taskAwardList);
+        var_dump($taskAwardList);
+        return count($taskAwardList);
+    }
+    
+    /**
+     * 获取任务奖励列表
+     * @param type $workerId
+     * @param type $finance_settle_apply_starttime
+     * @param type $finance_settle_apply_endtime
+     * @return type
+     */
+    public function getTaskAwardList($workerId,$finance_settle_apply_starttime,$finance_settle_apply_endtime){
+        return WorkerTask::getDoneTasksByWorkerId($finance_settle_apply_starttime, $finance_settle_apply_endtime, $workerId);
     }
     
     public function getWorkerOrderInfo($workerId){
