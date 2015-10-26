788cd2b58b986cbd732c9b206c7ba85d407a7f68
TianYuxing <tianyuxing@corp.1jiajie.com>
2015-10-26 21:43:02 +0800

    添加获取阿姨奖励列表接口

--- api/controllers/WorkerController.php
+++ api/controllers/WorkerController.php
@@ -739,6 +739,60 @@ class WorkerController extends \api\components\Controller
         ];
         return $this->send($ret, "操作成功.");
     }
+    
+    /**
+     * @api {GET} /worker/get-worker-taskreward-list 获取阿姨奖励列表 (田玉星 70%)
+     * 
+     * @apiDescription 【备注：等待model底层支持】
+     * 
+     * @apiName actionGetWorkerTaskrewardList
+     * @apiGroup Worker
+     * 
+     * @apiParam {String} access_token    阿姨登录token
+     * @apiParam {String} bill_id  账单唯一标识.
+     * @apiParam {String} [platform_version] 平台版本号.
+     * 
+     * @apiSampleRequest http://dev.api.1jiajie.com/v1/worker/get-worker-taskreward-list
+     * 
+     * @apiSuccessExample {json} Success-Response:
+     * HTTP/1.1 200 OK
+     * {
+     *   "code": 1,
+     *   "msg": "操作成功.",
+     *   "ret": [
+     *      {
+     *         "task_name": "单次抢单",
+     *         "order_price": "25.00",
+     *        }
+     *      ]
+     *   }
+     *
+     * @apiErrorExample Error-Response:
+     *  HTTP/1.1 404 Not Found
+     *  {
+     *      "code":"error",
+     *      "msg": "用户认证已经过期,请重新登录"
+     *  }
+     */
+    public function actionGetWorkerTaskrewardList(){
+        $param = Yii::$app->request->get() or $param =  json_decode(Yii::$app->request->getRawBody(),true);
+        //检测阿姨是否登录
+        $checkResult = $this->checkWorkerLogin($param);
+        if(!$checkResult['code']){
+            return $this->send(null, $checkResult['msg'], 0, 403);
+        }
+        //数据整理
+        $bill_id = intval($param['bill_id']);//账单ID
+        //获取任务奖励列表
+        $ret = [
+            [
+            "task_name" => "单次抢单",
+            "order_price" => "25.00",
+            ]
+        ];
+        return $this->send($ret, "操作成功.");
+    }
+    
     /**
      * @api {GET} /worker/get-worker-center 个人中心首页 (田玉星 100%)
      *
