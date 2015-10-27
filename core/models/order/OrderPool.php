0302e3bebd958b471fb38be3b301121bc74861c1
linhongyou <linhongyou@1jiajie.com>
2015-10-27 13:49:38 +0800

    添加了查询推送给阿姨的订单列表

--- core/models/order/OrderPool.php
+++ core/models/order/OrderPool.php
@@ -14,7 +14,71 @@ use yii\base\Model;
 
 class OrderPool extends Model
 {
-    const WAIT_ASSIGN_ORDERS_POOL = 'WaitAssignOrdersPool';
+    const WAIT_ASSIGN_ORDERS_POOL = 'WAIT_ASSIGN_ORDERS_POOL';
+    const PUSH_WORKER_ORDERS = 'PUSH_WORKER_ORDERS';
+    const PUSH_ORDER_WORKERS = 'PUSH_ORDER_WORKERS';
+
+    /**
+     * 添加订单到推送的阿姨列表
+     * @param $order_id
+     * @param $worker_id
+     */
+    public static function addOrderToWorkerPushList($order_id,$worker_id)
+    {
+        $order = OrderSearch::getOne($order_id);
+        $redis_order = [
+            'order_id' => $order_id,
+            'batch_code' => $order->order_batch_code,
+            'booked_begin_time' => $order->order_booked_begin_time,
+            'booked_end_time' => $order->order_booked_end_time,
+            'channel_name' => $order->order_channel_name,
+            'booked_count' => $order->order_booked_count,
+            'address' => $order->order_address,
+            'need' => $order->orderExtCustomer->order_customer_need
+        ];
+        Yii::$app->redis->executeCommand('zAdd', [self::PUSH_WORKER_ORDERS.'_'.$worker_id, $order_id, json_encode($redis_order)]);
+        Yii::$app->redis->executeCommand('zAdd', [self::PUSH_ORDER_WORKERS.'_'.$order_id, $worker_id, $worker_id]);
+    }
+
+    /**
+     * 从推送的阿姨列表中把订单删除
+     * @param $order_id
+     */
+    public static function remOrderForWorkerPushList($order_id)
+    {
+        $worker_ids = Yii::$app->redis->executeCommand('zRange', [self::PUSH_ORDER_WORKERS.'_'.$order_id, 0, -1]);
+        foreach($worker_ids as $worker_id) {
+            Yii::$app->redis->executeCommand('zRemRangeByScore', [self::PUSH_WORKER_ORDERS . '_' . $worker_id, $order_id, $order_id]);
+        }
+    }
+
+    /**
+     * 返回推送给阿姨的订单列表
+     * @param $worker_id
+     * @param int $page_size
+     * @param int $page
+     * @return mixed
+     */
+    public static function getOrdersFromWorkerPushList($worker_id,$page_size=20,$page=1)
+    {
+        $begin = ($page-1)*$page_size;
+        $end = $begin+$page_size-1;
+        return Yii::$app->redis->executeCommand('zRange', [self::PUSH_WORKER_ORDERS.'_'.$worker_id, $begin, $end]);
+
+    }
+
+    /**
+     * 返回推送给阿姨的订单总数
+     * @param $worker_id
+     * @return mixed
+     */
+    public static function getOrdersCountFromWorkerPushList($worker_id)
+    {
+
+        return Yii::$app->redis->executeCommand('zCard', [self::PUSH_WORKER_ORDERS.'_'.$worker_id]);
+
+    }
+
     /**
      * 把订单放入订单池
      * @param $order_id
@@ -30,7 +94,7 @@ class OrderPool extends Model
             'ivr' => $order->orderExtFlag->order_flag_worker_ivr,
             'worker_identity'=> $worker_identity,
         ];
-        Yii::$app->redis->executeCommand('zAdd', [self::WAIT_ASSIGN_ORDERS_POOL, $order->order_booked_begin_time . $order_id, json_encode($redis_order)]);
+        Yii::$app->redis->executeCommand('zAdd', [self::WAIT_ASSIGN_ORDERS_POOL, $order_id, json_encode($redis_order)]);
     }
 
     /**
@@ -39,13 +103,7 @@ class OrderPool extends Model
      */
     public static function remOrder($order_id)
     {
-        $orders = Yii::$app->redis->executeCommand('zrange', [self::WAIT_ASSIGN_ORDERS_POOL, 0, -1]);
-        foreach ($orders as $v) {
-            $redis_order_item = json_decode($v, true);
-            if ($redis_order_item['order_id'] == $order_id) {
-                Yii::$app->redis->executeCommand('zrem', [self::WAIT_ASSIGN_ORDERS_POOL, $v]);
-            }
-        }
+        Yii::$app->redis->executeCommand('zRemRangeByScore', [self::WAIT_ASSIGN_ORDERS_POOL, $order_id,$order_id]);
     }
 
     /**
