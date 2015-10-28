748e6b1a7671738c8a7143ba64f6514ce729096a
weibeinan <weibeinan@1jiajie.com>
2015-10-28 14:33:26 +0800

    解决阿姨端API的bug

--- core/models/finance/FinanceSettleApplySearch.php
+++ core/models/finance/FinanceSettleApplySearch.php
@@ -270,7 +270,19 @@ class FinanceSettleApplySearch extends FinanceSettleApply
 
     }
     
-    
+    public function getBaseSalarySubsidy($apply_order_money){
+        
+    }
+    /**
+     * 
+     */
+    public function isSelfAndFulltimeWorker($workerType,$workerIdentityId){
+        $isSelfAndFulltimeWorker = false;
+        if(($workerType == self::SELF_OPERATION) && ($workerIdentityId == self::FULLTIME)){
+            $isSelfAndFulltimeWorker = true;
+        }
+        return $isSelfAndFulltimeWorker;
+    }
     
     public function getWorkerOrderInfo($workerId){
         return  Order::find()->joinWith('orderExtWorker')->where(['orderExtWorker.worker_id'=>$workerId])->all();
@@ -289,8 +301,8 @@ class FinanceSettleApplySearch extends FinanceSettleApply
      * @param type $workerType
      * @param type $workerRuleId
      */
-    public function getSettleCycleByWorkerType($workerType,$workerRuleId){
-        if(($workerType == self::SELF_OPERATION) && ($workerRuleId == self::FULLTIME)){
+    public function getSettleCycleByWorkerType($workerType,$workerIdentityId){
+        if($this->isSelfAndFulltimeWorker($workerType, $workerIdentityId)){
             return self::FINANCE_SETTLE_APPLY_CYCLE_MONTH_DES;
         }else{
             return self::FINANCE_SETTLE_APPLY_CYCLE_WEEK_DES;
@@ -456,7 +468,7 @@ class FinanceSettleApplySearch extends FinanceSettleApply
     
     public static function getDeductionArrayBySettleId($settle_id){
       $deductionArray = FinanceWorkerNonOrderIncomeSearch::find()
-              ->select(['finance_worker_non_order_income as task_money','finance_worker_non_order_income_des as task_des'])
+              ->select(['finance_worker_non_order_income as deduction_money','finance_worker_non_order_income_des as deduction_des','finance_worker_non_order_income_type as deduction_type','FROM_UNIXTIME(finance_worker_non_order_income_complete_time,\'%Y.%m.%d\') as deduction_time'])
               ->where(['finance_settle_apply_id'=>$settle_id,'finance_worker_non_order_income_type'=>[FinanceWorkerNonOrderIncomeSearch::NON_ORDER_INCOME_DEDUCTION_COMPLAINT,FinanceWorkerNonOrderIncomeSearch::NON_ORDER_INCOME_DEDUCTION_COMPANSATE]])
               ->asArray()->all();
       return $deductionArray;
