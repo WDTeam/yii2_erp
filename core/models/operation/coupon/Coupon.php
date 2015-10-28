489dda7d6a3bd6e3a776738847d3517b04669921
liudaoqiang <liudaoqiang@corp1jiajie.com>
2015-10-28 22:27:43 +0800

    add functon get expirate time by coupon time type

--- core/models/operation/coupon/Coupon.php
+++ core/models/operation/coupon/Coupon.php
@@ -165,6 +165,42 @@ class Coupon extends \common\models\operation\coupon\Coupon
     }
 
 	/**
+     * get coupon code expirate_at by time type
+	 */
+	public static function getExpirateAtByTimeType($coupon_id, $get_at = time()){
+		$coupon = self::findOne($coupon_id);
+		if($coupon == NULL) return false;
+		
+		$expirate_at = 0;
+		switch ($coupon->coupon_time_type)
+		{
+			case 0;
+				if($get_at > $coupon->coupon_begin_at && $get_at < $coupon->coupon_end_at){
+					$expirate_at = $coupon->coupon_end_at;
+				}else{
+					return false;
+				}
+			break;
+		
+			case 1;
+				if($get_at > $coupon->coupon_begin_at && $get_at < $coupon_get_end_at){
+					$expirate_at =  $get_at + 24 * 3600 * $coupon->coupon_use_end_days;
+				}else{
+					return false;
+				}
+				
+			break;
+		
+			
+		
+			default:
+				# code...
+			break;
+		}
+		return $expirate_at;
+	}
+
+	/**
      * add coupon and coupon code
 	 */ 
 	public static function addCoupon($model){
