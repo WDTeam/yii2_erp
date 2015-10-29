61e4565c4c3e2489829285a21c0770c7e57b8ed1
fengfeng <kanghuifeng@corp.1jiajie.com>
2015-10-29 17:56:20 +0800

    表结构修改

--- console/migrations/m151026_074743_create_table_order_complaint.php
+++ console/migrations/m151026_074743_create_table_order_complaint.php
@@ -14,7 +14,7 @@ class m151026_074743_create_table_order_complaint extends Migration
     	
     	$this->createTable('{{%order_complaint}}',[
     			'id'=>  Schema::TYPE_PK.'(11) NOT NULL AUTO_INCREMENT COMMENT \'ID\'',
-    			'order_id'=>   Schema::TYPE_BIGPK.'(11) NOT NULL COMMENT \'订单id\'',
+    			'order_id'=>   Schema::TYPE_BIGINT.'(11) NOT NULL COMMENT \'订单id\'',
     			'complaint_type'=> Schema::TYPE_SMALLINT.'(2) DEFAULT 0 COMMENT \'投诉类型\'',
     			'complaint_status'=> Schema::TYPE_SMALLINT.'(2) DEFAULT 0 COMMENT \'投诉状态\'',
     			'complaint_channel'=> Schema::TYPE_SMALLINT.'(2) DEFAULT 0 COMMENT \'投诉渠道\'',
