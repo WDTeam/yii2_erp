e458c3c2aba41c94034f2cbe63a1f1c2111d9d21
colee <lidenggao@1jiajie.com>
2015-10-29 16:42:32 +0800

    coupon bind btn

--- boss/views/operation/coupon/coupon/index.php
+++ boss/views/operation/coupon/coupon/index.php
@@ -243,16 +243,20 @@ $this->params['breadcrumbs'][] = $this->title;
                         ]);
                     },
                     'bind' => function ($url, $model) {
-                        return Html::a('绑定', [
-                            'bind',
-                            'id' => $model->id
-                        ], [
-                            'title' => Yii::t('app', '绑定'),
-                            'data-toggle'=>'modal',
-                            'data-target'=>'#modal',
-                            'data-id'=>$model->id,
-                            'class'=>'bind-btn block-btn',
-                        ]);
+                        if($model->whetherCanBind()){
+                            return Html::a('绑定', [
+                                'bind',
+                                'id' => $model->id
+                            ], [
+                                'title' => Yii::t('app', '绑定'),
+                                'data-toggle'=>'modal',
+                                'data-target'=>'#modal',
+                                'data-id'=>$model->id,
+                                'class'=>'bind-btn block-btn',
+                            ]);
+                        }else{
+                            return false;
+                        }
                     },
                 ],
             ],
