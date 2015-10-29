ceb349d713709948d1e00ea6f555694d6034ed4e
Pangaofeng <pangaofeng@1jiajie.com>
2015-10-29 23:28:36 +0800

    修改渠道

--- boss/controllers/finance/FinancePopOrderController.php
+++ boss/controllers/finance/FinancePopOrderController.php
@@ -67,10 +67,12 @@ class FinancePopOrderController extends Controller
     			$data = \Yii::$app->request->post();
     			$model->load($data);
     			$file = UploadedFile::getInstance($model, 'finance_uplod_url');
-    			if(isset($file->baseName)){
+    			
+    			if(!isset($file->baseName)){
     				\Yii::$app->getSession()->setFlash('default','请上传对账单！');
     				return $this->redirect(['index']);
     			}
+    			
     			$filenamesitename=$file->baseName;
     			if($file){
     				$qiniu = new Qiniu();
