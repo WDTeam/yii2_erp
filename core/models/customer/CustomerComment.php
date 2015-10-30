73883b38e69c69567a8b58a8372194c3cf769c1a
Pangaofeng <pangaofeng@1jiajie.com>
2015-10-30 18:51:52 +0800

    修改评价第三方等级

--- core/models/customer/CustomerComment.php
+++ core/models/customer/CustomerComment.php
@@ -55,13 +55,15 @@ class CustomerComment extends \dbbase\models\customer\CustomerComment
     public static function addUserSuggest($array)
     {
 
+    	
         if (count($array) > 0) {
+        	$dateinfo=OrderComplaint::ComplaintTypes();
+        	$dateinfo_key=$dateinfo[1];
             $transaction = \Yii::$app->db->beginTransaction();
             try {
                 $customerComment = new CustomerComment;
                 $customerComment->order_id = $array['order_id'];
                 $customerComment->customer_id = $array['customer_id'];
-
                 $customerComment->worker_id = $array['worker_id'];
                 $customerComment->worker_tel = $array['worker_tel'];
                 $customerComment->operation_shop_district_id = $array['operation_shop_district_id']; //商圈id
@@ -69,7 +71,6 @@ class CustomerComment extends \dbbase\models\customer\CustomerComment
                 $customerComment->city_id = $array['city_id'];
                 $customerComment->county_id = $array['county_id'];
                 $customerComment->customer_comment_phone = $array['customer_comment_phone'];
-                ;
                 $customerComment->customer_comment_content = $array['customer_comment_content'];
                 $customerComment->customer_comment_level = $array['customer_comment_level'];
                 $customerComment->customer_comment_level_name = $array['customer_comment_level_name'];
@@ -86,15 +87,18 @@ class CustomerComment extends \dbbase\models\customer\CustomerComment
                 if ($array['customer_comment_level'] == '3') {
                     //如果是差评 通知投诉接口
                     $data['order_id'] = $array['order_id'];
-                    $data['worker_id'] = $array['worker_id'];
                     $data['complaint_type'] = 1;
-                    $data['complaint_status'] = 0;
-                    $data['complaint_channel'] = 0;
-                    $data['complaint_phone'] = $array['worker_tel'];
-                    $data['complaint_section'] = 0;
-                    $data['complaint_level'] = 3;
+                    $data['complaint_status'] = 1;
+                    $data['complaint_channel'] = 1;
+                    $data['complaint_phone'] = $array['customer_comment_phone'];
+                    $data['complaint_section'] = 1;
+                    $data['complaint_assortment'] =array_search($array['customer_comment_level_name'],$dateinfo_key);
+                    $data['complaint_level'] = 0;
                     $data['complaint_content'] = $array['customer_comment_content'];
                     $data['complaint_time'] = time();
+                    $data['updated_at'] = time();
+                    $data['created_at'] = time();
+                    $data['is_softdel'] = 1;
                     //提交给投诉接口
                     OrderComplaint::appModel($data);
                 }
@@ -109,61 +113,29 @@ class CustomerComment extends \dbbase\models\customer\CustomerComment
         }
     }
 
-    public static function issetes($array)
+    
+    /**
+    *  根据评价的order_id获取评价详情
+    * @date: 2015-10-30
+    * @author: peak pan
+    * @return:
+    **/
+   
+    public static function issetes($order_id)
     {
 
         $transaction = \Yii::$app->db->beginTransaction();
-        try {
-            $customerComment = new CustomerComment;
-            $customerComment->order_id = $array['order_id'];
-            $customerComment->customer_id = $array['customer_id'];
-
-            $customerComment->worker_id = $array['worker_id'];
-            $customerComment->worker_tel = $array['worker_tel'];
-            $customerComment->operation_shop_district_id = $array['operation_shop_district_id']; //商圈id
-            $customerComment->province_id = $array['province_id'];
-            $customerComment->city_id = $array['city_id'];
-            $customerComment->county_id = $array['county_id'];
-            $customerComment->customer_comment_phone = $array['customer_comment_phone'];
-            $customerComment->customer_comment_content = $array['customer_comment_content'];
-            $customerComment->customer_comment_level = $array['customer_comment_level'];
-            $customerComment->customer_comment_level_name = $array['customer_comment_level_name'];
-            $customerComment->customer_comment_tag_ids = $array['customer_comment_tag_ids'];
-            $customerComment->customer_comment_tag_names = $array['customer_comment_tag_names'];
-            $customerComment->customer_comment_anonymous = $array['customer_comment_anonymous'];
-            $customerComment->created_at = time();
-            $customerComment->updated_at = time();
-            $customerComment->is_del = 1;
-            $customerComment->save();
-            // var_dump($customerComment->errors);
-            $transaction->commit();
-
-            if ($array['customer_comment_level'] == '3') {
-                //如果是差评 通知投诉接口
-                $data['order_id'] = $array['order_id'];
-                $data['worker_id'] = $array['worker_id'];
-                $data['complaint_type'] = 1;
-                $data['complaint_status'] = 2;
-                $data['complaint_channel'] = 1;
-                $data['complaint_phone'] = $array['worker_tel'];
-                $data['complaint_section'] = 0;
-                $data['complaint_level'] = 3;
-                $data['complaint_content'] = $array['customer_comment_content'];
-                $data['complaint_time'] = time();
-                //提交给投诉接口
-                OrderComplaint::appModel($data);
-                $customerComment->validate(); //安全验证
-                $customer_info = $customerComment::findOne($customerComment);
-                if ($customer_info) {
-                    return $customer_info;
-                } else {
-                    return false;
-                }
-            }
-        } catch (\Exception $e) {
+	     try {
+            $condition['order_id']=$order_id;
+            $date=self::find()->where($condition)->one();
+            return $date;
+            
+       	} catch (\Exception $e) {
+       		
             $transaction->rollback();
             return false;
-        }
+            
+	    }
     }
 
 }
