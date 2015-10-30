2cea0744d91b7ea1fedd72907378035642784a60
zhaoshunli <zhaoshunli@corp.1jiajie.com>
2015-10-30 14:30:52 +0800

    更新apidoc文档

--- restapi/api-doc/api-html/api_project.js
+++ restapi/api-doc/api-html/api_project.js
@@ -14,7 +14,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-10-30T03:50:10.729Z",
+    "time": "2015-10-30T06:30:24.188Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
