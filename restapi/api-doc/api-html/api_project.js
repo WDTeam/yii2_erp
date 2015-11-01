97973ae66133d92824af3c81090f29262a305502
xieyi <xieyi@1jiajie.com>
2015-11-01 20:14:04 +0800

    update doc

--- restapi/api-doc/api-html/api_project.js
+++ restapi/api-doc/api-html/api_project.js
@@ -1,21 +1,1 @@
-define({
-  "name": "REST API",
-  "version": "0.0.0",
-  "description": "这里描述了所有客户端需要访问的API，任何的API访问请参考这里的内容",
-  "header": {
-    "title": "My own header title",
-    "content": "<h1>这里是描述文档的内容</h1>\n<h2>我们所有的API划归为几类，分别是</h2>\n<ol>\n<li>用户： user</li>\n<li>订单： order</li>\n<li>支付:  pay</li>\n</ol>\n"
-  },
-  "footer": {
-    "title": "My own footer title",
-    "content": "<p>Footer内容</p>\n"
-  },
-  "sampleUrl": false,
-  "apidoc": "0.2.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2015-10-31T06:54:02.638Z",
-    "url": "http://apidocjs.com",
-    "version": "0.13.1"
-  }
-});
+define({  "name": "REST API",  "version": "0.0.0",  "description": "这里描述了所有客户端需要访问的API，任何的API访问请参考这里的内容",  "header": {    "title": "My own header title",    "content": "<h1>这里是描述文档的内容</h1>\n<h2>我们所有的API划归为几类，分别是</h2>\n<ol>\n<li>用户： user</li>\n<li>订单： order</li>\n<li>支付:  pay</li>\n</ol>\n"  },  "footer": {    "title": "My own footer title",    "content": "<p>Footer内容</p>\n"  },  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2015-11-01T12:13:40.552Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
