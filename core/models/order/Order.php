0faa5c7da824bb097428b8e1855ed8a9d7f3d5e5
linhongyou <linhongyou@1jiajie.com>
2015-11-01 16:49:42 +0800

    处理异常

--- core/models/order/Order.php
+++ core/models/order/Order.php
@@ -532,6 +532,9 @@ class Order extends OrderModel
         if (empty($goods)) {
             $this->addError('order_service_type_name', '创建时获商品信息失败！');
             return false;
+        }elseif($goods['code']>=500){
+            $this->addError('order_service_type_name', $goods['msg']);
+            return false;
         }
         $this->setAttributes([
             'order_unit_money' => $goods['operation_shop_district_goods_price'], //单价
