68a9a445d90c72b2ea9a78568217021bad290096
xieyi <xieyi@1jiajie.com>
2015-11-02 17:38:06 +0800

    update OrderStatusDict reference from core

--- core/models/order/OrderSearch.php
+++ core/models/order/OrderSearch.php
@@ -5,7 +5,7 @@ namespace core\models\order;
 use dbbase\models\order\OrderExtCustomer;
 use dbbase\models\order\OrderExtFlag;
 use dbbase\models\order\OrderExtStatus;
-use dbbase\models\order\OrderStatusDict;
+use core\models\order\OrderStatusDict;
 use core\models\customer\Customer;
 use Yii;
 use yii\base\Model;
