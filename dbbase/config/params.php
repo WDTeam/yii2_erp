8a6db6e6c43344788790c1d27def442e906771e4
linhongyou <linhongyou@1jiajie.com>
2015-11-05 02:10:57 +0800

    调整了配置

--- dbbase/config/params.php
+++ dbbase/config/params.php
@@ -17,10 +17,5 @@ return [
         'allowZero' => false,
         'allowNegative' => false,
     ],
-    'order'=>[
-        'MANUAL_ASSIGN_lONG_TIME'=>900,
-        'ORDER_BOOKED_WORKER_ASSIGN_TIME'=>900,
-        'ORDER_FULL_TIME_WORKER_SYS_ASSIGN_TIME'=>300,
-        'ORDER_PART_TIME_WORKER_SYS_ASSIGN_TIME'=>900,
-    ]
+
 ];
