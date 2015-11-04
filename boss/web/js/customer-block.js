f10a3dcee1638130e36ae0ede6ad1514e3882c3f
liudaoqiang <liudaoqiang@corp1jiajie.com>
2015-11-04 22:52:28 +0800

    show customer tips while submit

--- boss/web/js/customer-block.js
+++ boss/web/js/customer-block.js
@@ -2,11 +2,11 @@ $(document).on("click",".btn-block",function(){
     //alert('asdfasdf');
     var block_reason = $('#block_reason').val();
     if(block_reason == ""){
-        $('#block_error').text('不能为空');
+        $('#block_error').text('请输入加入黑名单原因');
         exit;
     }
     if(block_reason.length > 300){
-        $('#block_error').text('不能超过300字符');
+        $('#block_error').text('输入黑名单原因不能超过300字符');
         exit;
     }
     
