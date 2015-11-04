abda36e3f01177e7863a661cc71ec3c91a343f11
Pangaofeng <pangaofeng@1jiajie.com>
2015-11-04 17:40:43 +0800

    解决禅道验证问题

--- boss/views/customer/customer-comment-tag/_form.php
+++ boss/views/customer/customer-comment-tag/_form.php
@@ -34,7 +34,7 @@ $rty=OrderComplaint::ComplaintTypes();
     		'options'=>['placeholder'=>'Enter 阿姨身份...','class'=>'test'],
     		'items'=>$rty['1'],
     		],		
-'customer_tag_name'=>['type'=> Form::INPUT_TEXT, 'options'=>['placeholder'=>'评价标签名称...批量用tag1|tag2|tag3|tag4', 'maxlength'=>255]], 
+'customer_tag_name'=>['type'=> Form::INPUT_TEXT, 'options'=>['placeholder'=>'评价标签名称...批量tag1|tag2|tag3|tag4', 'maxlength'=>100]], 
     		
     		'is_online'=>['type'=> Form::INPUT_RADIO_LIST, 'items'=>['1' => '开启', '2' => '关闭'],
     		'options'=>[
