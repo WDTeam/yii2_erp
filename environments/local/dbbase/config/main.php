43880c5c31fb87de68fe281216ef652ac24041d0
colee <lidenggao@1jiajie.com>
2015-11-05 18:32:49 +0800

    update env main config

--- environments/local/dbbase/config/main.php
+++ environments/local/dbbase/config/main.php
@@ -1,12 +1,6 @@
 <?php
-<<<<<<< HEAD
 
 return [
-=======
-error_reporting(E_ALL);
-$config =  [
-    
->>>>>>> 3be389533dd99eca4f15605ff5a0c3d815e12cbf
     'name' => 'BOSS',
     'language' => 'zh-CN',
     'timeZone' => 'Asia/Shanghai',
@@ -254,7 +248,6 @@ $config =  [
         ],
     ],
     'params'=> [
-<<<<<<< HEAD
         'adminEmail' => 'admin@1jiajie.com',
         'supportEmail' => 'support@1jiajie.com',
         'user.passwordResetTokenExpire' => 3600,
@@ -281,29 +274,5 @@ $config =  [
             'ORDER_FULL_TIME_WORKER_SYS_ASSIGN_TIME'=>300,
             'ORDER_PART_TIME_WORKER_SYS_ASSIGN_TIME'=>900,
         ]
-    ]
+    ], 
 ];
-=======
-    'adminEmail' => 'admin@1jiajie.com',
-    'supportEmail' => 'support@1jiajie.com',
-    'user.passwordResetTokenExpire' => 3600,
-    /**
-     * 配置财务数字显示
-     *  add by zhanghang 2015-09-22
-     */
-    'maskMoneyOptions' => [
-        'prefix' => '¥ ', // ¥ 在HTML，“¥”的命名实体是“&yen;”，x字符代码是“&#165;”和“&#xA5;”;
-        'suffix' => '',
-        'affixesStay' => true,
-        'thousands' => ',',
-        'decimal' => '.',
-        'precision' => 2, 
-        'allowZero' => false,
-        'allowNegative' => false,
-    ],
-
-]
-    
-];
-return $config;
->>>>>>> 3be389533dd99eca4f15605ff5a0c3d815e12cbf
