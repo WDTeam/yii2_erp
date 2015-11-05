f6b98a692cba240adadedc3664bb092215cbecaa
robertguo <robertguo@xlb.com>
2015-11-05 18:57:31 +0800

    修改了RestAPI的配置文件

---
+++ dbbase/config/dbbase-config.php
@@ -0,0 +1,9 @@
+<?php
+
+Yii::setAlias('dbbase', dirname(__DIR__));
+Yii::setAlias('boss', dirname(dirname(__DIR__)) . '/boss');
+Yii::setAlias('autoassign', dirname(dirname(__DIR__)) . '/autoassign');
+//Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('restapi', dirname(dirname(__DIR__)) . '/restapi');
+Yii::setAlias('core', dirname(dirname(__DIR__)) . '/core');
+Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
