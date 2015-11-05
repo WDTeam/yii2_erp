c01b24c589ef1e02f1413c85b4b2be2dae1bf01e
zhengyi <zhengyi@1jiajie.com>
2015-11-05 17:30:54 +0800

    配置文件合并

--- boss/web/index.php
+++ boss/web/index.php
@@ -7,7 +7,6 @@ require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../../dbbase/config/bootstrap.php');
 require(__DIR__ . '/../config/bootstrap.php');
-require(__DIR__ . '/../config/dbbase-local.php');
 
 $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../../dbbase/config/main.php'),
