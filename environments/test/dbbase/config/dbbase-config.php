48312eb9ac03d3ff73ff6e93f8b0987defdadded
colee <lidenggao@1jiajie.com>
2015-11-05 20:48:07 +0800

    update restapi

--- environments/test/dbbase/config/dbbase-config.php
+++ environments/test/dbbase/config/dbbase-config.php
@@ -23,8 +23,21 @@ return [
             'port' => 6379,
             'database' => 0,
         ],
+        'mongodb' => [
+            'class' => '\yii\mongodb\Connection',
+            'dsn' => 'mongodb://101.200.179.70:27017/boss_dev',
+        ],
         /**
          * 极光推送,默认为开发环境配置
+         * //正式
+         * $app_key='507d4a12d19ebbab7205f6bb';
+         * $master_secret = '30d1653625e797b7f80b56bb';
+         * // 测试
+         * $app_key='6b79c45db3ed3aa1706778f9';
+         * $master_secret = '7bcba44668a3ff6469fb57a5';
+         * //dev
+         * $app_key='3037ca7c859cca4c996f7144';
+         * $master_secret = 'a064811d7e4596c32d0e6884';
          */
         'jpush'=>[
             'class'=>'dbbase\components\JPush',
@@ -44,7 +57,7 @@ return [
         */
         'ivr'=>[
             'class'=>'dbbase\components\Ivr',
-            'app_id'=>'5000058',
+            'app_id'=>'5000040',
             'token'=>'57b62a3462b52a1413a4e1934a60d983',
             'redirect_uri'=>'system/ivr/callback'
         ],
--- environments/test/restapi/config/restapi-config.php
+++ environments/test/restapi/config/restapi-config.php
@@ -1,7 +1,72 @@
 <?php
-
 $config = [
+    'id' => 'app-api',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'aliases'=>[
+        '@api'=>'@app',
+    ],
+    'controllerNamespace' => 'api\controllers',
+    'modules' => [
+//         'user' => [
+//             'class' => 'api\modules\user\Module',
+//         ],
+    ],
     'components' => [
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+//            'enableStrictParsing' => true,
+            'showScriptName' => false,
+            'rules' => [
+                'v<version:\d>/<controller:\w+>/<id:\d+>' => '<controller>/view',
+                'v<version:\d>/<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                'v<version:\d>/<controller:\S+>/<action:\S+>' => '<controller>/<action>',
+                //'v<version:\d>/<controller:\S+>' => '<controller>',
+                [
+                    'class' => 'yii\rest\UrlRule',
+                    'controller' => 'user-info',
+                    'except' => ['delete'],
+                ],
+            ],
+        ],
+        'user' => [
+            'identityClass' => 'common\models\UserInfo',
+            'enableAutoLogin' => true,
+            'enableSession' => false,
+            'loginUrl' => null,
+        ],
+
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'ZMtQdfNPstwSBet6Kq11PxRnRHU8Qq6d',
+        ],
+        'response' => [
+            'format' => \yii\web\Response::FORMAT_JSON, //make json as default format
+        ],
+
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                'file' => [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['trace'],
+                    'categories' => ['yii\*'],
+                ],
+                'db' => [
+                    'class' => 'yii\log\DbTarget',
+                    'levels' => ['info'],
+                    'categories' => ['api-post'],
+                ],
+                'email' => [
+                    'class' => 'yii\log\EmailTarget',
+                    'levels' => ['error', 'warning'],
+                    'message' => [
+                        'to' => ['linhongyou@1jiajie.com'],
+                        'subject' => 'Exception Message',
+                    ],
+                ],
+            ],
+        ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'useFileTransport' =>true,//这句一定有，false发送邮件，true只是生成邮件在runtime文件夹下，不发邮件
@@ -21,17 +86,19 @@ $config = [
             ],
         ],
     ],
-    'service'=>[
-        'user'=>[
-            'domain'=>'http://dev.service.1jiajie.com:80/'
+    'params' =>[
+        'service'=>[
+            'user'=>[
+                'domain'=>'http://dev.service.1jiajie.com:80/'
+            ]
+        ],
+        'order'=>[
+            'MANUAL_ASSIGN_lONG_TIME'=>900,
+            'ORDER_BOOKED_WORKER_ASSIGN_TIME'=>900,
+            'ORDER_FULL_TIME_WORKER_SYS_ASSIGN_TIME'=>300,
+            'ORDER_PART_TIME_WORKER_SYS_ASSIGN_TIME'=>900,
         ]
     ],
-    'order'=>[
-        'MANUAL_ASSIGN_lONG_TIME'=>900,
-        'ORDER_BOOKED_WORKER_ASSIGN_TIME'=>900,
-        'ORDER_FULL_TIME_WORKER_SYS_ASSIGN_TIME'=>300,
-        'ORDER_PART_TIME_WORKER_SYS_ASSIGN_TIME'=>900,
-    ]
 ];
 
 if (YII_ENV_DEV) {
