f50d40cd2356d4b6133570062f956d36ca11e605
李勇 <liyong@corp.1jiajie.com>
2015-11-06 17:18:42 +0800

    修改备注

--- restapi/models/Worker.php
+++ restapi/models/Worker.php
@@ -13,11 +13,11 @@ class Worker
      */
     public static function checkWorkerLogin($param=array()){
         if(!isset($param['access_token'])||!$param['access_token']){
-           return array('code'=>0,'msg'=> alertMsgEnum::workerLoginFailed,'workerInfo'=>array('worker_id'=>0,'worker_is_block'=>0,'worker_identity_id'=>0));
+           return array('code'=>401,'msg'=> alertMsgEnum::workerLoginFailed,'workerInfo'=>array('worker_id'=>0,'worker_is_block'=>0,'worker_identity_id'=>0));
         }
         try{
             if(!WorkerAccessToken::checkAccessToken($param['access_token'])){
-                return array('code'=>0,'msg'=>alertMsgEnum::workerLoginFailed,'workerInfo'=>array('worker_id'=>0,'worker_is_block'=>0,'worker_identity_id'=>0));
+                return array('code'=>401,'msg'=>alertMsgEnum::workerLoginFailed,'workerInfo'=>array('worker_id'=>0,'worker_is_block'=>0,'worker_identity_id'=>0));
             }
             $workerObject = WorkerAccessToken::getWorker($param['access_token']);
             return array('code'=>1,'msg'=>  alertMsgEnum::workerLoginSuccess,'workerInfo'=>array('worker_id'=>$workerObject->id,'worker_is_block'=>$workerObject->worker_is_block,'worker_identity_id'=>$workerObject->worker_identity_id));
