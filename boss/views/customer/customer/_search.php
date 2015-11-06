b3a33b43b82c9b7ddf75c0e194a1e15ccafc5395
liudaoqiang <liudaoqiang@corp1jiajie.com>
2015-11-06 11:53:11 +0800

    customer end-date of created_at search

--- boss/views/customer/customer/_search.php
+++ boss/views/customer/customer/_search.php
@@ -84,7 +84,7 @@ use core\models\customer\Customer;
     </div>
 
     <div class='col-md-2'>
-        <?php echo $form->field($model, 'customer_global_search')->label('客户手机'); ?>
+        <?php echo $form->field($model, 'customer_global_search')->label('客户手机, '); ?>
     </div>
 
     <?php //echo $form->field($model, 'id') ?>
