4d2cc98d29b411453ce0156b980e4dff1a2bdd07
weibeinan <weibeinan@1jiajie.com>
2015-11-07 19:54:50 +0800

    发布记录

--- environments/test/boss/config/boss-config.php
+++ environments/test/boss/config/boss-config.php
@@ -55,7 +55,7 @@ $config =  [
     ]
 ];
 
-if (!YII_ENV_TEST) {
+if (!YII_DEBUG) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = 'yii\debug\Module';
