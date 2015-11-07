5e49f3c63a8e9ad5239805570c23d0245cae1989
linhongyou <linhongyou@1jiajie.com>
2015-11-07 17:48:54 +0800

    调整了BOSS创建订单显示

--- boss/controllers/order/OrderController.php
+++ boss/controllers/order/OrderController.php
@@ -342,7 +342,6 @@ class OrderController extends BaseAuthController
         } else {//init
             $model->order_booked_count = 2; //服务时长初始值2小时
             $model->order_booked_worker_id = 0; //不指定阿姨
-            $model->orderBookedTimeRange = '08:00-10:00';//预约时间段初始值
             $model->order_pay_type = 1;//支付方式 初始值
             $model->order_flag_sys_assign = 1;//是否系统指派
         }
