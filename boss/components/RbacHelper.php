d0d0d3dbb7858fd9e8ec5151182ef8284d16767b
colee <lidenggao@1jiajie.com>
2015-11-08 14:51:36 +0800

    update user info

--- boss/components/RbacHelper.php
+++ boss/components/RbacHelper.php
@@ -5,15 +5,45 @@ use yii\base\Component;
 class RbacHelper extends Component
 {
     /**
+     * 递归初始化菜单
+     */
+    private static function recursiveInitMenu($menus)
+    {
+        $auth = \Yii::$app->authManager;
+        foreach ($menus as $key=>$menu){
+            $url = $menu['url'];
+            if(isset($menu['can'])){
+                $name = $menu['can'];
+            }elseif (isset($url[0]) && $url[0]!='#'){
+                $name = $url[0];
+            }
+            if(!empty($name)){
+                $permission = $auth->getPermission($name);
+                if(empty($permission)){
+                    $permission = $auth->createPermission($name);
+                    $permission->description = $menu['label'];
+                    $auth->add($permission);
+                    $admin = $auth->getRole('super_admin');
+                    $auth->addChild($admin, $permission);
+                }
+                $menu['visible'] = \Yii::$app->user->can($name);
+            }
+            if(isset($menu['items'])){
+                $menu['items'] = self::recursiveInitMenu($menu['items']);
+            }
+            $menus[$key] = $menu;
+        }
+        return $menus;
+    }
+    /**
      * 处理菜单
      * @param array $menus
      */
     public static function menu($menus)
     {
-        $auth = \Yii::$app->authManager;
-        foreach ($menus as $menu){
-            
-        }
+        $menus = self::recursiveInitMenu($menus);
+        \Yii::$app->cache->set('boss-menus', $menus);
         return $menus;
     }
+    
 }
