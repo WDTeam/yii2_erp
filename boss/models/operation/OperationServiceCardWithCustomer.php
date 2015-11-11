739fb53b92090900fb2bccb9206c081867215d47
李飞 <lifei@corp.1jiajie.com>
2015-11-11 13:59:48 +0800

    服务卡数据字典修改

--- boss/models/operation/OperationServiceCardWithCustomer.php
+++ boss/models/operation/OperationServiceCardWithCustomer.php
@@ -10,16 +10,16 @@ use Yii;
  * @property string $id
  * @property string $service_card_sell_record_id
  * @property string $service_card_sell_record_code
- * @property string $server_card_info_id
+ * @property string $service_card_info_id
  * @property string $service_card_with_customer_code
- * @property string $server_card_info_name
+ * @property string $service_card_info_name
  * @property string $customer_trans_record_pay_money
- * @property string $server_card_info_value
+ * @property string $service_card_info_value
  * @property string $service_card_info_rebate_value
  * @property string $service_card_with_customer_balance
  * @property integer $customer_id
  * @property string $customer_phone
- * @property integer $server_card_info_scope
+ * @property integer $service_card_info_scope
  * @property integer $service_card_with_customer_buy_at
  * @property integer $service_card_with_customer_valid_at
  * @property integer $service_card_with_customer_activated_at
@@ -38,9 +38,9 @@ class OperationServiceCardWithCustomer extends \core\models\operation\OperationS
     public function rules()
     {
         return [
-            [['service_card_sell_record_id', 'server_card_info_id', 'customer_id', 'server_card_info_scope', 'service_card_with_customer_buy_at', 'service_card_with_customer_valid_at', 'service_card_with_customer_activated_at', 'service_card_with_customer_status', 'created_at', 'updated_at', 'is_del'], 'integer'],
-            [['customer_trans_record_pay_money', 'server_card_info_value', 'service_card_info_rebate_value', 'service_card_with_customer_balance'], 'number'],
-            [['service_card_sell_record_code', 'service_card_with_customer_code', 'server_card_info_name'], 'string', 'max' => 64],
+            [['service_card_sell_record_id', 'service_card_info_id', 'customer_id', 'service_card_info_scope', 'service_card_with_customer_buy_at', 'service_card_with_customer_valid_at', 'service_card_with_customer_activated_at', 'service_card_with_customer_status', 'created_at', 'updated_at', 'is_del'], 'integer'],
+            [['customer_trans_record_pay_money', 'service_card_info_value', 'service_card_info_rebate_value', 'service_card_with_customer_balance'], 'number'],
+            [['service_card_sell_record_code', 'service_card_with_customer_code', 'service_card_info_name'], 'string', 'max' => 64],
             [['customer_phone'], 'string', 'max' => 11]
         ];
     }
@@ -54,16 +54,16 @@ class OperationServiceCardWithCustomer extends \core\models\operation\OperationS
             'id' => Yii::t('app', 'id'),
             'service_card_sell_record_id' => Yii::t('app', '购卡销售记录id'),
             'service_card_sell_record_code' => Yii::t('app', '购卡订单号'),
-            'server_card_info_id' => Yii::t('app', '服务卡信息id'),
+            'service_card_info_id' => Yii::t('app', '服务卡信息id'),
             'service_card_with_customer_code' => Yii::t('app', '服务卡号'),
-            'server_card_info_name' => Yii::t('app', '服务卡卡名'),
+            'service_card_info_name' => Yii::t('app', '服务卡卡名'),
             'customer_trans_record_pay_money' => Yii::t('app', '实收金额'),
-            'server_card_info_value' => Yii::t('app', '卡面金额'),
+            'service_card_info_value' => Yii::t('app', '卡面金额'),
             'service_card_info_rebate_value' => Yii::t('app', '优惠金额'),
             'service_card_with_customer_balance' => Yii::t('app', '余额'),
             'customer_id' => Yii::t('app', '持卡人id'),
             'customer_phone' => Yii::t('app', '持卡人手机号'),
-            'server_card_info_scope' => Yii::t('app', '使用范围'),
+            'service_card_info_scope' => Yii::t('app', '使用范围'),
             'service_card_with_customer_buy_at' => Yii::t('app', '购买日期'),
             'service_card_with_customer_valid_at' => Yii::t('app', '有效截止日期'),
             'service_card_with_customer_activated_at' => Yii::t('app', '激活日期'),
