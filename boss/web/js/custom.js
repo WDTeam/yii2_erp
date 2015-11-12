0821375ca652b17a5343761ff82d5bc39e1579d1
fanyuanhe <fanyaunhe@corp.1jiajie.com>
2015-11-12 19:15:10 +0800

    城市管理前端页面修改

--- boss/web/js/custom.js
+++ boss/web/js/custom.js
@@ -85,7 +85,7 @@ $(document).ready(function(){
     var $btn_tabradio =$("div.btn_ipo .over_flow label");
     var $tab_text =$("div.tab_text");
     var $btn_tab_box =$("div.btn_tab_box");
-    var $reverse_box =$(".reverse");
+    var $reverse_box =$("#reverse");
 
     $div_li.click(function(){
         $(this).addClass("selected").siblings().removeClass("selected");
@@ -139,10 +139,19 @@ $(document).ready(function(){
     	}
     });
 
-    $reverse_box.each(function(){
-    	$(this).click(function(){
-            $(this).siblings().addClass("selected");
-    	})
+    $reverse_box.click(function(){
+    	if($(this).siblings().hasClass("selected")){
+    		$(this).click(function(){
+    			$(this).siblings().removeClass("selected");
+    			$(this).removeClass("selected");
+    		});
+    	}else{
+    		$(this).click(function(){
+    			$(this).siblings().addClass("selected");
+    			$(this).addClass("selected");
+    		});
+    	}
+        
     });
 
     //--------------------------------------------------------------------------------------结束
