cfbe02769e0517bdd3c5f6500a397e220358ebf1
colee <lidenggao@1jiajie.com>
2015-11-12 20:22:25 +0800

    update core qiniu

--- core/models/operation/OperationGoods.php
+++ core/models/operation/OperationGoods.php
@@ -5,7 +5,7 @@ namespace core\models\operation;
 use Yii;
 use boss\models\operation\OperationShopDistrictGoods;
 use yii\web\UploadedFile;
-use crazyfd\qiniu\Qiniu;
+
 
 /**
  * This is the model class for table "{{%operation_goods}}".
@@ -82,12 +82,11 @@ class OperationGoods extends \dbbase\models\operation\OperationGoods
      * @return string $imgUrl 文件URL
      */
     public function uploadImgToQiniu($field){
-        $qiniu = new Qiniu();
         $fileinfo = UploadedFile::getInstance($this, $field);
         if(!empty($fileinfo)){
             $key = time().mt_rand('1000', '9999').uniqid();
-            $qiniu->uploadFile($fileinfo->tempName, $key);
-            $imgUrl = $qiniu->getLink($key);
+            \Yii::$app->imageHelperuploadFile($fileinfo->tempName, $key);
+            $imgUrl = \Yii::$app->imageHelpergetLink($key);
             $this->$field = $imgUrl;
         }
     }
