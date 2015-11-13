2e91294355f7ab49523a810424b3ebe3e79108b3
万连波 <wanlianbo@corp.1jiajie.com>
2015-11-13 22:30:21 +0800

    增加输入框的限制

--- boss/views/operation/operation-platform/_form.php
+++ boss/views/operation/operation-platform/_form.php
@@ -12,7 +12,7 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'operation_platform_name')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'operation_platform_name')->textInput(['maxlength' => 10]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
