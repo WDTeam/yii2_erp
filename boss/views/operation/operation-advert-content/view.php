a8177cba5273969888b8afc3769321ae84099c62
万连波 <wanlianbo@corp.1jiajie.com>
2015-11-15 17:33:12 +0800

    修改bug:广告内容详情页不需要显示活动开始时间，活动结束时间，广告上线时间，广告下线时间字段。广告图片不应该以链接形式出现，应该为预览图

--- boss/views/operation/operation-advert-content/view.php
+++ boss/views/operation/operation-advert-content/view.php
@@ -3,6 +3,8 @@
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 
+use boss\models\operation\OperationCommon;
+
 /* @var $this yii\web\View */
 /* @var $model boss\models\Operation\OperationAdvertContent */
 
@@ -31,13 +33,11 @@ $this->params['breadcrumbs'][] = $this->title;
         'attributes' => [
             'id',
             'operation_advert_content_name',
-//            'operation_city_id',
-//            'operation_city_name',
-            'operation_advert_start_time:date',
-            'operation_advert_end_time:date',
-            'operation_advert_online_time:date',
-            'operation_advert_offline_time:date',
-            'operation_advert_picture_text',
+            [
+                'attribute'=>'operation_advert_picture_text',
+                'format' => ['image',['width'=>'30','height'=>'30',]],
+                'value' => $model->operation_advert_picture_text
+            ],
             'operation_advert_url:url',
             'created_at:datetime',
             'updated_at:datetime',
