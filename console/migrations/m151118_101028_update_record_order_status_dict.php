ceae23cbe65cbecdbbcf3b7733d742dc84c53851
linhongyou <linhongyou@1jiajie.com>
2015-11-18 18:18:02 +0800

    修改状态字典记录

--- console/migrations/m151118_101028_update_record_order_status_dict.php
+++ console/migrations/m151118_101028_update_record_order_status_dict.php
@@ -12,6 +12,6 @@ class m151118_101028_update_record_order_status_dict extends Migration
 
     public function safeDown()
     {
-        $this->update('{{%order_status_dict}}',['order_status_customer'=>'指派中'],['order_status_customer'=>'待指派']);
+        $this->update('{{%order_status_dict}}',['order_status_customer'=>'指派中'],['order_status_boss'=>['人工派单失败','人工派单中','智能分单中','智能指派中']]);
     }
 }
