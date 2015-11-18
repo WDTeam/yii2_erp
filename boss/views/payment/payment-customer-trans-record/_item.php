e12585e2b21fabd49cf813dc8a36e4785740c969
lishengqiang <lishengqiang@1jiajie.com>
2015-11-18 15:58:52 +0800

    修改交易记录样式

--- boss/views/payment/payment-customer-trans-record/_item.php
+++ boss/views/payment/payment-customer-trans-record/_item.php
@@ -11,7 +11,7 @@ use boss\models\order\Order;
         line-height: 27px;
     }
     .table{
-        width:98%;
+        width:95%;
     }
     .list-view{
         padding:18px;
@@ -38,13 +38,13 @@ use boss\models\order\Order;
              <td colspan="5">
                 用户手机号码：<?= Html::encode($model->customer_phone) ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
-                交易方式：<?= Html::encode($model->payment_customer_trans_record_mode_name) ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-                订单渠道：<?= Html::encode($model->payment_customer_trans_record_order_channel) ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
                 E家洁订单编号：<?= Html::encode($model->order_code) ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
                 E家洁周期订单号：<?= Html::encode($model->order_batch_code) ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                交易方式：<?= Html::encode($model->payment_customer_trans_record_mode_name) ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                订单渠道：<?= Html::encode($model->payment_customer_trans_record_order_channel) ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
                 用户累计交易额：<span><?= Html::encode($model->payment_customer_trans_record_total_money) ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <a class="last" href="<?= Yii::$app->urlManager->createUrl(['payment/payment-customer-trans-record/view', 'id' => $model->id])?>">查看详情</a>
@@ -74,17 +74,18 @@ use boss\models\order\Order;
             </td>
             <td width="20%">
                 <div class="pay_record"><br/>
-                <div class="pay_record">商户订单号：<span><?= Html::encode($model->payment_customer_trans_record_eo_order_id) ?></span></div>
-                <div class="pay_record">交易流水号：<span><?= Html::encode($model->payment_customer_trans_record_transaction_id) ?></span></div>
-                <div class="pay_record">在线支付：<span><?= Html::encode($model->payment_customer_trans_record_online_pay) ?></span></div>
                 <div class="pay_record">支付渠道：<span><?= Html::encode($model->payment_customer_trans_record_pay_channel) ?></span></div>
+                <div class="pay_record">商户订单号：<span><?= Html::encode($model->payment_customer_trans_record_eo_order_id) ?></span></div>
+                <div class="pay_record">在线支付：<span><?= Html::encode($model->payment_customer_trans_record_online_pay) ?></span></div>
+                <div class="pay_record">交易流水号：<span><?= Html::encode($model->payment_customer_trans_record_transaction_id) ?></span></div>
             </td>
             <td width="20%">
-                <div class="pay_record">退款金额：<span><?= Html::encode($model->payment_customer_trans_record_refund_money) ?></span></div>
+                <div class="pay_record">现金支付：<span><?= Html::encode($model->payment_customer_trans_record_cash) ?></span></div>
                 <div class="pay_record">预付费金额(第三方)：<span><?= Html::encode($model->payment_customer_trans_record_pre_pay) ?></span></div>
                 <div class="pay_record">补偿金额：<span><?= Html::encode($model->payment_customer_trans_record_compensate_money) ?></span></div>
                 <div class="pay_record">订单总金额：<span><?= Html::encode($model->payment_customer_trans_record_order_total_money) ?></span></div>
-                <div class="pay_record">现金支付：<span><?= Html::encode($model->payment_customer_trans_record_cash) ?></span></div>
+                <div class="pay_record">退款金额：<span><?= Html::encode($model->payment_customer_trans_record_refund_money) ?></span></div>
+
 
             </td>
         </tr>
